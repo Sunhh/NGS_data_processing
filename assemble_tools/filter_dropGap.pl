f910a717da93cc7e2e03153a0e1854611dfb249a
Sunhh <biosunhh@gmail.com>
2014-07-28 16:54:59 -0400

    Edit filter_dropGap.pl to get back useful scaffolds back.

--- assemble_tools/filter_dropGap.pl
+++ assemble_tools/filter_dropGap.pl
@@ -1,8 +1,10 @@
 #!/usr/bin/perl 
 use strict; 
 
-my $min_len = 150; 
-my $max_gap = 700; 
+my $trim_tail = 70; 
+my $min_scf_len = 500; 
+my $max_gap = 500; 
+my $min_tail_len = 300; 
 
 my (%seq, $kk); 
 while (<>) {
@@ -15,18 +17,36 @@ while (<>) {
 }
 
 my $sc_ct = 0; 
+SEQ: 
 for (keys %seq) {
+	$_ =~ m/^DropGap/ or next; 
 	$seq{$_} =~ s/\s//g; 
-	$seq{$_} =~ s/^[nN]+//; 
-	$seq{$_} =~ s/[nN]+$//; 
-	length($seq{$_}) < $min_len and next; 
+	while ( $seq{$_} =~ s/^[ATCGatgc]{0,$trim_tail}[nN]+//o ) { 1; }
+	while ( $seq{$_} =~ s/[nN]+[ATGCatgc]{0,$trim_tail}$//o ) { 1; }
+	length($seq{$_}) < $min_scf_len and next; 
 	my @a1 = split(/[nN]{$max_gap,}/, $seq{$_}); 
 	for my $ss (@a1) {
-		length($ss) < $min_len and next; 
+		while ( $ss =~ s/^[ATCGatgc]{0,$trim_tail}[nN]+//o ) { 1; } 
+		while ( $ss =~ s/[nN]+[ATGCatgc]{0,$trim_tail}$//o ) { 1; }
+		length($ss) < $min_scf_len and next; 
+		if ( $ss =~ m/^([ATGCatgc]+)[nN]+([ATGCatgc]+)$/ ) {
+			my ($seqH, $seqT) = ($1, $2); 
+			if ( length($seqH) < $min_tail_len or length($seqT) < $min_tail_len) {
+				for my $s1 ($seqH, $seqT) {
+					length( $s1 ) < $min_scf_len and next; 
+					$sc_ct ++; 
+					my $tseq = $s1; 
+					$tseq =~ s/(.{100})/$1\n/g; chomp($tseq); 
+					print ">dropGap.$sc_ct [$_]\n$tseq\n"; 
+				}
+				next SEQ; 
+			}
+		}
+		
 		$sc_ct ++; 
 		my $tseq = $ss; 
 		$tseq =~ s/(.{100})/$1\n/g; chomp($tseq); 
-		print ">$sc_ct [$_]\n$tseq\n"; 
+		print ">dropGap.$sc_ct [$_]\n$tseq\n"; 
 	}
 }
 
