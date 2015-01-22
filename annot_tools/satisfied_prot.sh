97ae05078c6b638eb70be2ff990041e9ca7d6c00
Sunhh <biosunhh@gmail.com>
2015-01-22 15:36:30 -0500

    More feasible.

--- annot_tools/satisfied_prot.sh
+++ annot_tools/satisfied_prot.sh
@@ -9,8 +9,12 @@ function tsmsg {
 	echo "[$(date)]$1"
 }
 
-pl_bpTrans=/home/laopopo/tools/github/NGS_data_processing/bp0_2_bp6.pl
-pl_slctGff=/home/laopopo/tools/github/NGS_data_processing/assemble_tools/slct_maker_gff3.pl
+pl_bpTrans=$HOME/tools/github/NGS_data_processing/bp0_2_bp6.pl
+pl_slctGff=$HOME/tools/github/NGS_data_processing/assemble_tools/slct_maker_gff3.pl
+
+dbPath=prot_db/uniprot_sprot_plants.fas
+dbTag="Sprot"
+cpuN=50
 
 maxDist2Edge=9
 
@@ -20,9 +24,9 @@ inProtFa="r${rN}_maker.prot.fa"
 inGff="r${rN}_maker.gff3"
 outGff="r${rN}_maker_good.gff3"
 
-exe_cmd "blastp -evalue 1e-10 -query $inProtFa -db prot_db/uniprot_sprot_plants.fas -num_threads 50 -out ${inProtFa}.toSprot.bp0"
-exe_cmd "perl $pl_bpTrans -in ${inProtFa}.toSprot.bp0 -out ${inProtFa}.toSprot.bp6"
-exe_cmd "awk ' \$7 <= $maxDist2Edge+1 && \$9 <= $maxDist2Edge+1 && \$10 >= \$14-$maxDist2Edge && \$8 >= \$13-$maxDist2Edge && \$3 >= 60 ' ${inProtFa}.toSprot.bp6 > ${inProtFa}.toSprot.bp6.good" 
-exe_cmd "perl $pl_slctGff ${inProtFa}.toSprot.bp6.good $inGff > $outGff"
+exe_cmd "blastp -evalue 1e-10 -query $inProtFa -db $dbPath -num_threads $cpuN -out ${inProtFa}.to${dbTag}.bp0"
+exe_cmd "perl $pl_bpTrans -in ${inProtFa}.to${dbTag}.bp0 -out ${inProtFa}.to${dbTag}.bp6"
+exe_cmd "awk ' \$7 <= $maxDist2Edge+1 && \$9 <= $maxDist2Edge+1 && \$10 >= \$14-$maxDist2Edge && \$8 >= \$13-$maxDist2Edge && \$3 >= 60 ' ${inProtFa}.to${dbTag}.bp6 > ${inProtFa}.to${dbTag}.bp6.good" 
+exe_cmd "perl $pl_slctGff ${inProtFa}.to${dbTag}.bp6.good $inGff > $outGff"
 
 
