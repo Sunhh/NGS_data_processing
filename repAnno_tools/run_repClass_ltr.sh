a4fc94487bfb54e094d6bf0d058276b349692c70
Sunhh <biosunhh@gmail.com>
2015-01-31 16:51:11 +0000

    Shell for classify ltr and mite with repeatmodeler.

---
+++ repAnno_tools/run_repClass_ltr.sh
@@ -0,0 +1,12 @@
+### Basic functions.
+function exe_cmd {
+	echo "[$(date)][CMD] $1"
+	eval $1
+	echo "[$(date)][Rec] Done."
+}
+
+function tsmsg {
+	echo "[$(date)]$1"
+}
+
+exe_cmd "/share/app/Annotation/repeatmodeler/RepeatModeler/RepeatClassifier -consensi allLTR_rmGen_chop.fa -engine ncbi"
