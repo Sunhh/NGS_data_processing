74fd01aebad545abde62b5fe41ba5eaabad37526
Sunhh <biosunhh@gmail.com>
2015-02-06 14:30:56 -0500

    Add comments

--- MyPM/mathSunhh.pm
+++ MyPM/mathSunhh.pm
@@ -144,7 +144,7 @@ sub max {
 #              BWA estimates the insert size distribution per 256*1024 read pairs. It first collects pairs of reads with both ends mapped with a single-end quality 20 or higher and then calculates median (Q2), lower and higher quartile (Q1 and Q3). It estimates the mean and the variance of the insert size distribution from pairs whose insert sizes are within interval [Q1-2(Q3-Q1), Q3+2(Q3-Q1)]. The maximum distance x for a pair considered to be properly paired (SAM flag 0x2) is calculated by solving equation Phi((x-mu)/sigma)=x/L*p0, where mu is the mean, sigma is the standard error of the insert size distribution, L is the length of the genome, p0 is prior of anomalous pair and Phi() is the standard cumulative distribution function. For mapping Illumina short-insert reads to the human genome, x is about 6-7 sigma away from the mean. Quartiles, mean, variance and x will be printed to the standard error output.
 # Input      : (\@ins_value_array)
 # Output     : (\%hash_of_values) 
-#              keys = qw(Q1 Q3 interval_low interval_high interval_mean interval_median interval_var interval_stdev limit_low limit_high)
+#              keys = qw(SUM COUNT MEAN MEDIAN Q1 Q3 interval_low interval_high interval_mean interval_median interval_var interval_stdev limit_low limit_high)
 sub ins_calc {
 	my $r_arr = shift; 
 	my $min_val_number = shift // 1; 
