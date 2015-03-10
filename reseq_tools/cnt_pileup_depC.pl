d6eef7bda77a43de09a6c21bda27a9aa695ff472
Sunhh <biosunhh@gmail.com>
2015-03-10 06:14:15 -0400

    Count not N bases' depth histogram in pileup file.

---
+++ reseq_tools/cnt_pileup_depC.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+use strict; 
+use warnings; 
+
+use LogInforSunhh; 
+
+for (@ARGV) {
+	&exeCmd("awk '\$3 != \"N\" { print \$4 }' $_ | deal_table.pl -col_repCount 0 | deal_table.pl -col_sort 1 > $_.noN.depC &"); 
+}
