939f71ae65d19e975cef283cf7ceb7bac0c96ad7
Sunhh <biosunhh@gmail.com>
2015-03-14 06:41:44 -0400

    N length could be changed now.

--- Proc_Reads/srch_barcode.pl
+++ Proc_Reads/srch_barcode.pl
@@ -10,11 +10,13 @@ my $r1pat_set3 = 'ATCTCGTATGCCGTCTTCTGCTTG';
 my $r1pat_set1 = 'AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC'; 
 
 my $r2pat = 'AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT'; 
+my $n1_len=40;
+my $n2_len=41; 
 
 for my $f (@ARGV) {
 	my $cnt_found = 0; 
-	my %cnt_n40; 
-	my %cnt_n41; 
+	my %cnt_n1; 
+	my %cnt_n2; 
 	my $fh = &openFH($f, '<'); 
 	while (my $id = <$fh>) {
 		$cnt_found >= 10000 and last; 
@@ -22,44 +24,44 @@ for my $f (@ARGV) {
 		<$fh>; <$fh>; 
 		if ( $seq =~ m/$r1pat_set3/o ) {
 			my $is = 0; 
-			if ( $seq =~ m/(.{40}$r1pat_set3)/o ) {
-				$cnt_n40{$1} ++; 
+			if ( $seq =~ m/(.{$n1_len}$r1pat_set3)/o ) {
+				$cnt_n1{$1} ++; 
 				$is = 1; 
 			}
-			if ( $seq =~ m/(.{41}$r1pat_set3)/o ) {
-				$cnt_n41{$1} ++; 
+			if ( $seq =~ m/(.{$n2_len}$r1pat_set3)/o ) {
+				$cnt_n2{$1} ++; 
 				$is = 1; 
 			}
 			$is == 1 and $cnt_found ++; 
 		}
 	}
-	my @k_n40 = sort { $cnt_n40{$b} <=> $cnt_n40{$a} } keys %cnt_n40; 
-	my @k_n41 = sort { $cnt_n41{$b} <=> $cnt_n41{$a} } keys %cnt_n41; 
+	my @k_n1 = sort { $cnt_n1{$b} <=> $cnt_n1{$a} } keys %cnt_n1; 
+	my @k_n2 = sort { $cnt_n2{$b} <=> $cnt_n2{$a} } keys %cnt_n2; 
 	
-	print STDERR "prevNbp=40\n"; 
+	print STDERR "prevNbp=$n1_len\n"; 
 	for (my $i=0; $i<5; $i++) {
-		defined $k_n40[$i] or next; 
-		print STDERR join("\t", $k_n40[$i], $cnt_n40{ $k_n40[$i] })."\n"; 
+		defined $k_n1[$i] or next; 
+		print STDERR join("\t", $k_n1[$i], $cnt_n1{ $k_n1[$i] })."\n"; 
 	}
-	print STDERR "prevNbp=41\n"; 
+	print STDERR "prevNbp=$n2_len\n"; 
 	for (my $i=0; $i<5; $i++) {
-		defined $k_n41[$i] or next; 
-		print STDERR join("\t", $k_n41[$i], $cnt_n41{ $k_n41[$i] })."\n"; 
+		defined $k_n2[$i] or next; 
+		print STDERR join("\t", $k_n2[$i], $cnt_n2{ $k_n2[$i] })."\n"; 
 	}
-	my $has_n40 = 0; 
-	if ( defined $k_n40[1] and $cnt_n40{ $k_n40[0] } > 3 * $cnt_n40{ $k_n40[1] } ) {
-		print STDERR "Found R1_pattern with N40 : \t$k_n40[0]\n"; 
-		$has_n40 = 1; 
+	my $has_n1 = 0; 
+	if ( defined $k_n1[1] and $cnt_n1{ $k_n1[0] } > 3 * $cnt_n1{ $k_n1[1] } ) {
+		print STDERR "Found R1_pattern with N$n1_len : \t$k_n1[0]\n"; 
+		$has_n1 = 1; 
 	}
-	my $has_n41 = 0; 
-	if ( defined $k_n41[1] and $cnt_n41{ $k_n41[0] } > 3 * $cnt_n41{ $k_n41[1] } ) {
-		print STDERR "Found R1_pattern with N41 : \t$k_n41[0]\n"; 
-		$has_n41 = 1; 
+	my $has_n2 = 0; 
+	if ( defined $k_n2[1] and $cnt_n2{ $k_n2[0] } > 3 * $cnt_n2{ $k_n2[1] } ) {
+		print STDERR "Found R1_pattern with N$n2_len : \t$k_n2[0]\n"; 
+		$has_n2 = 1; 
 	}
-	if ( $has_n40 == 1 and $has_n41 == 0 ) {
-		print STDERR "Good R1_pattern : \t$k_n40[0]\n"; 
-		print STDOUT join("\t", $f, $k_n40[0])."\n"; 
-		if ( $k_n40[0] =~ m!^${r1pat_set1}(\S+)${r1pat_set3}$! ) {
+	if ( $has_n1 == 1 and $has_n2 == 0 ) {
+		print STDERR "Good R1_pattern : \t$k_n1[0]\n"; 
+		print STDOUT join("\t", $f, $k_n1[0])."\n"; 
+		if ( $k_n1[0] =~ m!^${r1pat_set1}(\S+)${r1pat_set3}$! ) {
 			print STDERR "Good barcode : \t$1\n"; 
 		}
 	}
