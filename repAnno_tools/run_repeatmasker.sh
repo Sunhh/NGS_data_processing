fe6df1dd2cb077289f882f2d1d9cbbe740649048
Sunhh <biosunhh@gmail.com>
2015-03-16 23:01:13 -0400

    Add buildSummary.pl

--- repAnno_tools/run_repeatmasker.sh
+++ repAnno_tools/run_repeatmasker.sh
@@ -10,14 +10,21 @@ function tsmsg {
 }
 
 exe_rpmk="/data/Sunhh/src/Annot/repeatmasker/RepeatMasker/RepeatMasker"
+pl_buildSum='/data/Sunhh/src/Annot/repeatmasker/RepeatMasker/util/buildSummary.pl'
 
-cpuN=10
-repLib="KnownRepeats_v1.lib"
+cpuN=20
+repLib="allRepeats_v1.lib"
 seqfile="P1All.scf.fa"
 minLen=100
 
 tsmsg "[Rec] All start."
 
-exe_cmd "$exe_rpmk -s -x -lib $repLib $seqfile -nolow -norna -no_is -pa $cpuN -a 1>stdout.RepMsk_known 2>stderr.RepMsk_known"
+# exe_cmd "$exe_rpmk -s -x -lib $repLib $seqfile -nolow -norna -no_is -pa $cpuN -a 1>stdout.RepMsk_unknown 2>stderr.RepMsk_unknown"
+tsv_genom='P1Genom_Gt5h.scf.fa.tsv'
+exe_cmd "perl $pl_buildSum -useAbsoluteGenomeSize -genome $tsv_genom $seqfile.out > $seqfile.out.NuclGenom.summary"
+
+tsmsg "[Rec] All done."
+
+
 
 tsmsg "[Rec] All done."
