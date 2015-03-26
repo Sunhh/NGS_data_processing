0a19595237278a1e3d2b13cc2573747e08a93a8b
Sunhh <biosunhh@gmail.com>
2015-03-26 17:15:35 -0400

    Add In_count and Unknown_count columns.

--- classify_tools/get_Ex_region.pl
+++ classify_tools/get_Ex_region.pl
@@ -6,7 +6,9 @@ my $ms = mathSunhh->new();
 
 -t and !@ARGV and die "perl $0 P1R02scaf.nt_bn6.MiORGN_join 1>sep_ex_lis 2>joined_ex_lis\n"; 
 
-my %merged_blk; 
+my %merged_blk; # Blocks for Ex class 
+my %merged_in_blk; # Blocks for In class
+my %merged_un_blk; # Blocks for Un-defined class that including In and Ex both. 
 my %lines; 
 while (<>) {
 	chomp; 
@@ -19,18 +21,49 @@ while (<>) {
 		$en = $1; 
 	}
 
-	$en > 0 or next; 
-	
-	if ($in == 0) {
-		; 
-	} elsif ($in <= 3) {
-		$en >= $in+3 or next; 
+	my $is_in = -1; 
+	my $is_ex = -1; 
+
+	if ( $en > 0 ) {
+		if ( $in == 0 ) {
+			$is_ex = 1; 
+		} elsif ( $in <= 3 ) {
+			if ( $en >= $in+3 ) {
+				$is_ex = 1; $is_in = 0; 
+			} elsif ( $in >= $en+3 ) {
+				$is_in = 1; $is_in = 0; 
+			} else {
+				$is_in = 0; $is_ex = 0; 
+			}
+		} else { 
+			if ( $en >= $in*2 ) {
+				$is_ex = 1; $is_in = 0; 
+			} elsif ( $in >= $en*2 ) {
+				$is_in = 1; $is_ex = 0; 
+			} else {
+				$is_in = 0; $is_ex = 0; 
+			}
+		}
+	} elsif ( $in > 0 ) {
+		$is_in = 1; $is_ex = 0; 
 	} else {
-		$en >= $in*2 or next; 
+		next; 
 	}
-	push(@{$merged_blk{$ta[0]}}, [$ta[2], $ta[3]]); 
-	push(@{$lines{$ta[0]}}, [@ta]); 
-	print "$_\n"; 
+
+	if ( $is_in == 1 ) {
+		push(@{$merged_in_blk{$ta[0]}}, [$ta[2], $ta[3]]); 
+	} elsif ( $is_ex == 1 ) {
+		push(@{$merged_blk{$ta[0]}}, [$ta[2], $ta[3]]); 
+		push(@{$lines{$ta[0]}}, [@ta]); 
+		print "$_\n"; 
+	} elsif ( $is_in == 0 and $is_ex == 0 ) {
+		push(@{$merged_un_blk{$ta[0]}}, [$ta[2], $ta[3]]); 
+	} elsif ( $is_in == -1 and $is_ex == -1 ) {
+		next; 
+	} else {
+		die "$_\n"; 
+	}
+
 }
 
 for my $tid (keys %merged_blk) {
@@ -39,8 +72,23 @@ for my $tid (keys %merged_blk) {
 	for my $tr (@$tar) {
 		$cnt_len += ($tr->[1]-$tr->[0]+1); 
 	}
+	my $cnt_len_in = 0; 
+	if (defined $merged_in_blk{$tid}) {
+		my $tar = $ms->mergeLocBlk( $merged_in_blk{$tid} ); 
+		for my $tr (@$tar) {
+			$cnt_len_in += ($tr->[1]-$tr->[0]+1); 
+		}
+	}
+	my $cnt_len_un = 0; 
+	if (defined $merged_un_blk{$tid}) {
+		my $tar = $ms->mergeLocBlk( $merged_un_blk{$tid} ); 
+		for my $tr (@$tar) {
+			$cnt_len_un += ($tr->[1]-$tr->[0]+1); 
+		}
+	}
+
 	my @tb = @{$lines{$tid}[0]}; 
-	print STDERR join("\t", $tid, $tb[1], $tar->[0][0], $tar->[-1][1], $cnt_len, @tb[5 .. $#tb])."\n"; 
+	print STDERR join("\t", $tid, $tb[1], $tar->[0][0], $tar->[-1][1], $cnt_len, @tb[5 .. $#tb], $cnt_len_in, $cnt_len_un)."\n"; 
 }
 
 
