a6ffa5cfa64325c6d0a7f27d6f48a7ae5f9c316a
Sunhh <biosunhh@gmail.com>
2015-06-04 10:26:28 -0400

    Kill processes by PPID.

---
+++ cmd_ctrl/kill_by_ppid.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+use strict; 
+use warnings; 
+use LogInforSunhh; 
+
+!@ARGV and die "perl $0 ppid\n"; 
+
+my $ppid = shift; 
+my @txt = `ps --ppid $ppid`; 
+for (@txt) {
+	chomp; 
+	m!^\s*(\d+)! or next; 
+	my $pid = $1; 
+	&tsmsg("kill -9 $pid"); 
+}
+
