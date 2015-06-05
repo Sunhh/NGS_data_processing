b9920d20d952a146e441735acc09bb7aad220f22
Sunhh <biosunhh@gmail.com>
2015-06-05 13:08:14 -0400

    Use geno_col

--- reseq_tools/cnt_homo_ratio.pl
+++ reseq_tools/cnt_homo_ratio.pl
@@ -4,6 +4,8 @@ use warnings;
 
 my $geno_col = 3; 
 
+!@ARGV and die "perl $0 appleSNP_rmClose.cols.set1_woSet2Set3.miss20_heteNotN > appleSNP_rmClose.cols.set1_woSet2Set3.miss20_heteNotN.homoR\nPlease note that the geno_col is $geno_col\n"; 
+
 my $l = <>; 
 print join("\t",qw/ChromID Pos HomoRatio/)."\n"; 
 while (<>) {
