48eda4218683847e4b76be66d7e0c7ede657db7e
Sunhh <biosunhh@gmail.com>
2015-08-26 17:11:23 -0400

    Use -both_aln to get both-end mapped read pairs under -max_NM_ratio threshold.

--- Proc_Sam/get_required_sam.pl
+++ Proc_Sam/get_required_sam.pl
@@ -6,9 +6,12 @@
 #                 Please note that here I don't filter insert size for well. 
 #   Purpose 3 : I want best-aligned read pairs with some (NM/read_length)% control; 
 #                 This is similar to -well_pair but filtering if XA_minNM <= NM ; 
+#   Purpose 4 : Both ends aligned, and filter the NM/XA_minNM ratio. 
+#                 I don't care if the two ends map to the same chrID; 
 use strict; 
 use warnings; 
 use SeqAlnSunhh; 
+use mathSunhh; 
 use LogInforSunhh; 
 use Getopt::Long; 
 my %opts; 
@@ -17,6 +20,7 @@ GetOptions(\%opts,
 	'uniq_pair!', # Purpose 1. 
 	'well_pair!', # Purpose 2. 
 	'best_pair!', # Purpose 3. 
+	'both_aln!', # Purpose 4. 
 	
 	# Local for 'well_pair'
 	
@@ -43,6 +47,8 @@ $opts{'min_mapQ'} //= 0;
 defined $opts{'help'} and &usage(); 
 if ( defined $opts{'uniq_pair'} or defined $opts{'well_pair'} or defined $opts{'best_pair'} ) {
 	&get_uniq_pair(); 
+} elsif ( defined $opts{'both_aln'} ) {
+	&get_both_aln(); 
 }
 
 
@@ -62,6 +68,9 @@ sub usage {
 # 
 # -well_pair      [Boolean] well-aligned read pairs with some (NM/read_length)% control; 
 # -best_pair      [Boolean] Same to -well_pair but filtering if XA_minNM <= NM ; 
+#
+# -both_aln       [Boolean] Both ends aligned, and filter the NM/XA_minNM ratio by parameter -max_NM_ratio ; 
+#                   I don't care if the two ends map to the same chrID; 
 # 
 # Global filtering: 
 # -max_NM_ratio   [-1] Maximum NM/rdLen ratio accepted. 
@@ -120,6 +129,53 @@ sub get_uniq_pair {
 		%bad_rd = (); 
 	}
 }# get_uniq_pair
+
+sub get_both_aln {
+	my %bad_rd; 
+	my %rd_cnt_r1; 
+	my %rd_cnt_r2; 
+	my @lines; 
+	my %paired_flag = %{ &SeqAlnSunhh::mk_flag('keep'=>'0=1,2=0,3=0') }; 
+	my %r1_flag = %{ &SeqAlnSunhh::mk_flag('keep'=>'0=1,2=0,3=0,6=1,7=0') }; 
+	my %r2_flag = %{ &SeqAlnSunhh::mk_flag('keep'=>'0=1,2=0,3=0,7=1,6=0') }; 
+	while (<>) {
+		$. % 100e3 == 1 and &tsmsg("[Msg] Processing $. line.\n"); 
+		m/^\@/ and do { print; next; }; 
+
+		chomp; 
+		my @ta = split(/\t/, $_); 
+		my $sam_href = &SeqAlnSunhh::sam_line2hash(\@ta ); 
+		defined $paired_flag{ $ta[1] } or next; 
+		my $rdID = $sam_href->{'qname'}; 
+		defined $bad_rd{ $rdID } and next; 
+		$sam_href->{'mapq'} >= $opts{'min_mapQ'} or do { $bad_rd{$rdID} = 1; next; }; 
+		if ($opts{'max_NM_ratio'} >= 0) {
+			&SeqAlnSunhh::sam_hash_addKey($sam_href, ['NM', 'read_len', 'XA_minNM']); 
+			my $vv = &mathSunhh::min( $sam_href->{'NM'}, $sam_href->{'XA_minNM'} ); 
+			$vv > $sam_href->{'read_len'} * $opts{'max_NM_ratio'} and do { $bad_rd{$rdID} = 1; next; }; 
+		}
+
+		if ( defined $r1_flag{$ta[1]} ) {
+			$rd_cnt_r1{$rdID} ++; 
+		} elsif ( defined $r2_flag{$ta[1]} ) {
+			$rd_cnt_r2{$rdID} ++; 
+		} else {
+			&stopErr("[Err] Bad flag [$ta[1]]\n"); 
+		}
+		push(@lines, [$rdID, $_]); 
+	}
+	&tsmsg("[Msg] Filtering reads for pairing\n"); 
+	if (@lines > 0) {
+		for my $rdID (keys %rd_cnt_r1) {
+			defined $bad_rd{$rdID} and next; 
+			defined $rd_cnt_r2{$rdID} or $bad_rd{$rdID} = 1; 
+		}
+		&SeqAlnSunhh::print_sam_lines(\@lines, \%bad_rd); 
+		@lines = (); 
+		%bad_rd = (); 
+		%rd_cnt_r1 = %rd_cnt_r2 = (); 
+	}
+}
 
 ################################################################################
 #      Inner Sub-routines
