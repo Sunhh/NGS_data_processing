edabd5bc621bcec41294db49fc3b3e7efe4dafc6
Sunhh <biosunhh@gmail.com>
2015-10-15 14:39:43 -0400

    Add hete_Ratio and homo_Ratio.

--- reseq_tools/cntNHH_byIndv.pl
+++ reseq_tools/cntNHH_byIndv.pl
@@ -20,7 +20,7 @@ perl $0 in.snp > in.snp.cntNHH
 Genotype column start from colN=$opts{'startColN'}
 Do not parse the first line. 
 
-The output format is : qw/IndvID N_Num Typed_Num Het_Num Hom_Num/
+The output format is : qw/IndvID N_Num Typed_Num Het_Num Hom_Num Het_Ratio Hom_Ratio/
 
 HH
 	exit(1); 
@@ -50,14 +50,16 @@ while (<>) {
 		# $ta[$i]=~m/^[ATGC*]$|^[ATGC]\+[ATGC]+$/ and $cnt[$i]++; 
 	} 
 } 
-print STDOUT join("\t", qw/IndvID N_Num Typed_Num Het_Num Hom_Num/)."\n"; 
+print STDOUT join("\t", qw/IndvID N_Num Typed_Num Het_Num Hom_Num Het_Ratio Hom_Ratio/)."\n"; 
 for (my $i=0; $i<@ha; $i++) { 
 	$cnt_N[$i] //= 0; 
 	$cnt_hete[$i] //= 0; 
 	$cnt_homo[$i] //= 0; 
 	my $tot = $cnt_N[$i]+$cnt_hete[$i]+$cnt_homo[$i]; 
 	my $tot_typed = $cnt_hete[$i]+$cnt_homo[$i]; 
-	print "$ha[$i]\t$cnt_N[$i]\t$tot_typed\t$cnt_hete[$i]\t$cnt_homo[$i]\n";
+	my $rat_hete = ($tot_typed > 0) ? sprintf("%02.20f", $cnt_hete[$i]/$tot_typed*100) : -1 ; 
+	my $rat_homo = ($tot_typed > 0) ? sprintf("%02.20f", $cnt_homo[$i]/$tot_typed*100) : -1 ; 
+	print "$ha[$i]\t$cnt_N[$i]\t$tot_typed\t$cnt_hete[$i]\t$cnt_homo[$i]\t$rat_hete\t$rat_homo\n";
 }
 
 
