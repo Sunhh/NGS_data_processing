a6f325deac0b9bcf38704f93b73af5d6cdf4dd02
Sunhh <biosunhh@gmail.com>
2015-10-15 14:33:41 -0400

    Count Nmissing Homo Hete counts for each site.

---
+++ reseq_tools/cntNHH_byIndv.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl
+use strict; 
+use warnings; 
+use LogInforSunhh; 
+use Getopt::Long; 
+my %opts; 
+GetOptions(\%opts, 
+	"help!", 
+	"startColN:i", # 2 
+); 
+$opts{'startColN'} //= 2; 
+
+my $type_startColN = $opts{'startColN'}; 
+sub usage {
+	print STDERR <<HH; 
+
+perl $0 in.snp > in.snp.cntNHH
+-startColN          [$opts{'startColN'}]
+
+Genotype column start from colN=$opts{'startColN'}
+Do not parse the first line. 
+
+HH
+	exit(1); 
+}
+
+-t and !@ARGV and &usage(); 
+$opts{'help'} and &usage(); 
+
+my $head = <>; 
+chomp($head); 
+my @ha=split(/\t/, $head); 
+my @cnt_N; 
+my @cnt_hete; 
+my @cnt_homo; 
+
+while (<>) { 
+	($.-1) % 1e6 == 1 and &tsmsg("[Msg] $. lines.\n"); 
+	chomp; 
+	my @ta = split(/\t/, $_); 
+	for (my $i=$type_startColN; $i<@ta; $i++) { 
+		$ta[$i] eq 'N' and $cnt_N[$i]++; 
+		if ($ta[$i] =~ m/^[ATGC*][ATGC*]$/) {
+			$cnt_hete[$i]++; 
+		} else {
+			$cnt_homo[$i]++; 
+		}
+		# $ta[$i]=~m/^[ATGC*]$|^[ATGC]\+[ATGC]+$/ and $cnt[$i]++; 
+	} 
+} 
+print STDOUT join("\t", qw/IndvID N_Num Typed_Num Het_Num Hom_Num/)."\n"; 
+for (my $i=0; $i<@ha; $i++) { 
+	$cnt_N[$i] //= 0; 
+	$cnt_hete[$i] //= 0; 
+	$cnt_homo[$i] //= 0; 
+	my $tot = $cnt_N[$i]+$cnt_hete[$i]+$cnt_homo[$i]; 
+	my $tot_typed = $cnt_hete[$i]+$cnt_homo[$i]; 
+	print "$ha[$i]\t$cnt_N[$i]\t$tot_typed\t$cnt_hete[$i]\t$cnt_homo[$i]\n";
+}
+
+
