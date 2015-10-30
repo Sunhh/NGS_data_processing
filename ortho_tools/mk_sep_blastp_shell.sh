ce1ec87264a355e85f3cd9ec87772c8826ff46aa
Sunhh <biosunhh@gmail.com>
2015-10-30 10:21:17 -0400

    Correct dir name.

--- ortho_tools/mk_sep_blastp_shell.sh
+++ ortho_tools/mk_sep_blastp_shell.sh
@@ -6,7 +6,7 @@ pref=$4
 if [ -n "$pref" ] 
 then
 	deal_fasta.pl $inFa -cut $cutN -cut_prefix $pref
-	ls $pref/*.fasta | perl -e ' while (<>) { chomp; print "blastp -query $_ -out $_.blast -db all.fa -evalue 1e-5 -max_target_seqs 1000 -outfmt 6 -num_threads 3 \n"; } ' > cmd_list_blastp_${pref}
+	ls ${pref}_cutted/*.fasta | perl -e ' while (<>) { chomp; print "blastp -query $_ -out $_.blast -db all.fa -evalue 1e-5 -max_target_seqs 1000 -outfmt 6 -num_threads 3 \n"; } ' > cmd_list_blastp_${pref}
 	echo "CMD:    nohup run_cmd_in_batch.pl -cpuN 60 cmd_list_blastp_${pref} > scrn.sep_${pref}"
 else
 	echo "bash $0    in.fa    db_name    cut_num    cut_pref"
