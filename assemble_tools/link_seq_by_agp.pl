710082828116cdf9b5efc915dc83b3703e32f3ee
Sunhh <biosunhh@gmail.com>
2015-11-05 13:51:17 -0500

    Add check step for positions in agp file.

--- assemble_tools/link_seq_by_agp.pl
+++ assemble_tools/link_seq_by_agp.pl
@@ -12,8 +12,11 @@ GetOptions(\%opts,
 	"agpFile:s", 
 	"ctgFas:s", 
 	"noAddRest!", 
-	"Nchar:s", 
+	"Nchar:s", "Uchar:s", 
 ); 
+$opts{'Nchar'} //= 'n'; 
+$opts{'Uchar'} //= 'n'; 
+
 my $help_txt = <<HH; 
 
 perl $0 -agpFile SP_Nov_11_13_30_8_90_3_superscaffold_merged_edit.agp -ctgFas Spo_V1p5.scaf.fa > Spo_V1p5_Jn.scaf.fa
@@ -21,7 +24,8 @@ perl $0 -agpFile SP_Nov_11_13_30_8_90_3_superscaffold_merged_edit.agp -ctgFas Sp
 -help 
 
 -noAddRest          [Bool] Only output sequences within .agp file if given. 
--Nchar              ['n'] Character to fill as gap. 
+-Nchar              ['$opts{'Nchar'}'] Character to fill gap for ele_N. 
+-Uchar              ['$opts{'Uchar'}'] Character to fill gap for ele_U. 
 
 HH
 
@@ -29,7 +33,6 @@ $opts{'help'} and &LogInforSunhh::usage($help_txt);
 for my $tk (qw/agpFile ctgFas/) {
 	defined $opts{$tk} or &LogInforSunhh::usage($help_txt); 
 }
-$opts{'Nchar'} //= 'n'; 
 my %orient = qw(
 +     P
 -     M
@@ -43,7 +46,7 @@ M     M
 ); 
 
 &tsmsg("[Msg] Loading AGP file [$opts{'agpFile'}].\n"); 
-my @agp_arr = @{ &load_agp_file( $opts{'agpFile'} ) }; 
+my @agp_arr = @{ &agp_sort( &load_agp_file( $opts{'agpFile'} ) ) }; 
 &tsmsg("[Msg] Loading ctg_fasta [$opts{'ctgFas'}].\n"); 
 my %ctg_fas = %{ $fas_obj->save_seq_to_hash('faFile'=>$opts{'ctgFas'}, 'has_head'=>1) }; 
 for (keys %ctg_fas) { $ctg_fas{$_}{'seq'} =~ s/\s//g; } 
@@ -55,10 +58,15 @@ my %used_ctg;
 for my $la (@agp_arr) {
 	my $newID = $la->[0]; 
 	defined $new_seq{$newID} or $new_seq{$newID}{'order'} = ++$seq_cnt; 
+	$new_seq{$newID}{'prevP'} //= 0; 
 	$new_seq{$newID}{'seq'} //= ''; 
 	my $add_seq = ''; 
-	if ( $la->[4] =~ m/^(N|U)$/ ) {
+	$la->[1] == $new_seq{$newID}{'prevP'} + 1 or &stopErr("[Err] The position coordinates are not continuous: scfID=$newID prevP=$new_seq{$newID}{'prevP'} currP=$la->[1]\n"); 
+	if ( $la->[4] =~ m/^N$/i ) {
 		$add_seq = $opts{'Nchar'} x $la->[5]; 
+		$new_seq{$newID}{'seq'} .= $add_seq; 
+	} elsif ( $la->[4] =~ m/^U$/i ) { 
+		$add_seq = $opts{'Uchar'} x $la->[5]; 
 		$new_seq{$newID}{'seq'} .= $add_seq; 
 	} elsif ( $la->[4] eq 'W') {
 		defined $ctg_fas{$la->[5]} or &stopErr("No ctg_fas for ID [$la->[5]]\n"); 
@@ -69,7 +77,7 @@ for my $la (@agp_arr) {
 	} else {
 		&stopErr("[Err] No rule for component_type[5] [$la->[4]] yet.\n"); 
 	}
-
+	$new_seq{$newID}{'prevP'} = $la->[2]; 
 }
 
 &tsmsg("[Msg] Output joined sequences.\n"); 
@@ -106,4 +114,16 @@ sub load_agp_file {
 	close($fh); 
 	return(\@back); 
 }
+
+sub agp_sort {
+	# $_[0] : \@agp_arr 
+	my @back; 
+	my %ord; 
+	my $cnt=0; 
+	for (@{$_[0]}) {
+		$ord{$_->[0]} //= ++$cnt; 
+	}
+	@back = sort { $ord{$a->[0]} <=> $ord{$b->[0]} || $a->[1] <=> $b->[1] || $a->[2] <=> $b->[2] } @{$_[0]}; 
+	return(\@back); 
+}# agp_sort() 
 
--- assemble_tools/resize_N_in_agp.pl
+++ assemble_tools/resize_N_in_agp.pl
@@ -33,16 +33,32 @@ for my $tk (qw/agpFile/) {
 }
 
 &tsmsg("[Msg] Loading AGP file [$opts{'agpFile'}].\n"); 
-my @agp_arr = @{ &load_agp_file( $opts{'agpFile'} ) }; 
+my @agp_arr = @{ &agp_sort( &load_agp_file( $opts{'agpFile'} ) ) }; # Sort AGP file for safety. 
 
+# [Sunhh@whale super_scaffold]$ head -3 SP_Nov_11_13_30_8_90_3_superscaffold_merged.agp
+# ##agp-version   2.0
+# Super_scaffold_248      1       341379  1       W       SpoScf_00639    1       341379  -
+# Super_scaffold_248      341380  600696  2       N       259317  scaffold        yes     map
 &tsmsg("[Msg] Output agp file.\n"); 
+my %info; 
 for my $la (@agp_arr) {
+	my $scfID = $la->[0]; 
+	$info{$scfID}{'prevP'} //= 0; 
 	if ( $la->[4] =~ m/^N$/i ) {
 		$la->[5] = $opts{'Nlen'}; 
+		$la->[1] = $info{$scfID}{'prevP'} + 1; 
+		$la->[2] = $info{$scfID}{'prevP'} + $la->[5]; 
+		$info{$scfID}{'prevP'} = $la->[2]; 
 	} elsif ( $la->[4] =~ m/^U$/i ) {
 		$la->[5] = $opts{'Ulen'}; 
+		$la->[1] = $info{$scfID}{'prevP'} + 1; 
+		$la->[2] = $info{$scfID}{'prevP'} + $la->[5]; 
+		$info{$scfID}{'prevP'} = $la->[2]; 
 	} elsif ( $la->[4] eq 'W' ) {
-		; 
+		my $ele_len = $la->[7]-$la->[6]+1; 
+		$la->[1] = $info{$scfID}{'prevP'} + 1; 
+		$la->[2] = $info{$scfID}{'prevP'} + $ele_len; 
+		$info{$scfID}{'prevP'} = $la->[2]; 
 	} else {
 		&stopErr("[Err] No rule for component_type[5] [$la->[4]] yet.\n"); 
 	}
@@ -50,6 +66,18 @@ for my $la (@agp_arr) {
 }
 
 &tsmsg("[Msg] Done $0.\n"); 
+
+sub agp_sort {
+	# $_[0] : \@agp_arr 
+	my @back; 
+	my %ord; 
+	my $cnt=0; 
+	for (@{$_[0]}) {
+		$ord{$_->[0]} //= ++$cnt; 
+	}
+	@back = sort { $ord{$a->[0]} <=> $ord{$b->[0]} || $a->[1] <=> $b->[1] || $a->[2] <=> $b->[2] } @{$_[0]}; 
+	return(\@back); 
+}# agp_sort() 
 
 # [Sunhh@whale super_scaffold]$ head -3 SP_Nov_11_13_30_8_90_3_superscaffold_merged.agp
 # ##agp-version   2.0
