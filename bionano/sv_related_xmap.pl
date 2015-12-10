4ecf38efd54fd375c7253719d2301dcd429b675b
Sunhh <biosunhh@gmail.com>
2015-12-10 13:34:24 -0500

    Again fix denominator computation for -maxCov threshold.

--- bionano/sv_related_xmap.pl
+++ bionano/sv_related_xmap.pl
@@ -37,12 +37,19 @@ while (<>) {
 
 	$ta[8] >= $opts{'minConfidence'} or next; 
 
+	my $str      = $ta[7]; 
 	my $cov_qLen = abs($ta[4]-$ta[3]); # I don't want add 1 because it shouldn't matter in the current length variations. 
 	my $cov_sLen = abs($ta[6]-$ta[5]); 
 	my $ttl_qLen = $ta[10]; 
 	my $ttl_sLen = $ta[11]; 
-	my $max_cov_qLen = &mathSunhh::min( &mathSunhh::max(@ta[3,4]), $ttl_qLen-&mathSunhh::min(@ta[3,4]) ); 
-	my $max_cov_sLen = &mathSunhh::min( &mathSunhh::max(@ta[5,6]), $ttl_qLen-&mathSunhh::min(@ta[5,6]) ); 
+	my $max_cov_qLen = ( $str eq '-' ) ? 
+	    &mathSunhh::min($ttl_qLen-$ta[3], $ta[5]-1) + $cov_qLen + &mathSunhh::min($ta[4]-1, $ttl_sLen-$ta[6]-1)  
+	  : &mathSunhh::min($ta[3]-1, $ta[5]-1) + $cov_qLen + &mathSunhh::min( $ttl_qLen-$ta[4]-1, $ttl_sLen-$ta[6]-1 ) ; 
+	my $max_cov_sLen = ( $str eq '-' ) ? 
+	    &mathSunhh::min($ttl_qLen-$ta[3], $ta[5]-1) + $cov_sLen + &mathSunhh::min($ta[4]-1, $ttl_sLen-$ta[6]-1)  
+	  : &mathSunhh::min($ta[3]-1, $ta[5]-1) + $cov_sLen + &mathSunhh::min( $ttl_qLen-$ta[4]-1, $ttl_sLen-$ta[6]-1 ) ; 
+	# my $max_cov_qLen = &mathSunhh::min( &mathSunhh::max(@ta[3,4]), $ttl_qLen-&mathSunhh::min(@ta[3,4]) ); 
+	# my $max_cov_sLen = &mathSunhh::min( &mathSunhh::max(@ta[5,6]), $ttl_qLen-&mathSunhh::min(@ta[5,6]) ); 
 	my $is_bad = 1; 
 	$cov_qLen >= $opts{'maxCov'} * $max_cov_qLen and $is_bad = 0; 
 	$cov_sLen >= $opts{'maxCov'} * $max_cov_sLen and $is_bad = 0; 
