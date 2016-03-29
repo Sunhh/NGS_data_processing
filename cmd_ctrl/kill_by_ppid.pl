0b7d471f755392d629fce436244d07411eaec5c4
Sunhh <biosunhh@gmail.com>
2016-03-29 17:11:58 -0400

    more detail .

--- cmd_ctrl/kill_by_ppid.pl
+++ cmd_ctrl/kill_by_ppid.pl
@@ -2,16 +2,42 @@
 use strict; 
 use warnings; 
 use LogInforSunhh; 
+use Getopt::Long; 
+my %opts; 
+GetOptions(\%opts,
+	"help!", 
+	"forAlluser!", 
+	"doWith_1!", 
+); 
 
-!@ARGV and die "perl $0 ppid curr_user\n"; 
+my $help_txt = <<HH; 
 
+perl $0 ppid_to_Kill 
+
+-help
+
+-forAlluser       [Boolean] If given, try to kill all child processes of input ppid. If not given, only consider within current session. 
+
+-doWith_1         [Boolean] Have to be given if ppid is 1. This is dangerous. 
+
+HH
+
+my $help_1 = <<AA;
+
+Input ppid is 1, please add -doWith_1 for continue. 
+
+AA
+
+!@ARGV and &LogInforSunhh::usage($help_txt); 
+$opts{'help'} and &LogInforSunhh::usage($help_txt); 
 my $ppid = shift; 
-my $curr = shift; 
-$curr //= 0; 
+$ppid == 1 and &LogInforSunhh::usage($help_1); 
+
 
 my @txt = `ps --ppid $ppid`; 
+
 my %cur; 
-if ( $curr ) {
+unless ( $opts{'forAlluser'} ) {
 	my @txt_cur = `ps`; 
 	for (@txt_cur) {
 		chomp; 
