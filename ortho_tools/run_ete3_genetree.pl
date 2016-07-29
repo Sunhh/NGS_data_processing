de657a772d036a3743593cd5c2caadbbbfbc828d
Sunhh <biosunhh@gmail.com>
2016-07-29 16:58:34 -0400

    Build gene-tree.

--- ortho_tools/run_ete3_genetree.pl
+++ ortho_tools/run_ete3_genetree.pl
@@ -1,8 +1,10 @@
 #!/usr/bin/perl
+# 20160729 Add multi-threading 
 use strict; 
 use warnings; 
 use LogInforSunhh; 
 use Getopt::Long; 
+use Parallel::ForkManager; 
 use fileSunhh; 
 use fastaSunhh; 
 my $fas_obj = fastaSunhh->new(); 
@@ -14,10 +16,12 @@ GetOptions(\%opts,
 	"max_geneN:i", # 9999 
 	"out_dir:s", # ete3_out/
 	"ete3_cmd:s", # 'ete3 build --cpu 25 -w phylomedb4 '
+	"cpuN:i", # 1
 ); 
 
 $opts{'ete3_cmd'}  //= 'ete3 build --cpu 25 -w phylomedb4 '; 
 $opts{'max_geneN'} //= 200; 
+$opts{'cpuN'}      //= 1; 
 
 my $help_txt = <<HH; 
 ##################################################################################################
@@ -25,8 +29,17 @@ my $help_txt = <<HH;
 #
 #   This tool is used to generate gene tree within each COG using ETE3 toolkit. 
 #
+# Reaured : 
+# -in_cog        [filename] Format: sp1_seqA1 \\t sp2_seqA2 \\t ... \\n sp1_seqB \\t sp2_seqB \\t ... \\n ...
+# -in_prot       [filename] fasta 
+# -out_dir       [DirName]  
+#
+# Optional : 
+#
 # -max_geneN     [$opts{'max_geneN'}] Maximal number of genes within a OG. 
 # -ete3_cmd      ['$opts{'ete3_cmd'}']
+#
+# -cpuN          [$opts{'cpuN'}] Multi-threading. 
 #
 # -help 
 #
@@ -51,6 +64,7 @@ my @cog_tab = &fileSunhh::load_tabFile( $opts{'in_cog'} ) ;
 my $wrk_dir = &fileSunhh::new_tmp_dir(); 
 mkdir($wrk_dir) or &stopErr("[Err] Failed to create dir [$wrk_dir]\n"); 
 
+
 $glob{'curr_order'} = 0; 
 for ( my $i=0; $i<@cog_tab; $i++ ) {
 	$glob{'curr_order'} = $i+1; 
@@ -58,11 +72,22 @@ for ( my $i=0; $i<@cog_tab; $i++ ) {
 	$curr_geneN > 0 or do { &tsmsg("[Wrn] Skip $glob{'curr_order'} -th COG because no gene found.\n"); next; }; 
 	$curr_geneN > $opts{'max_geneN'} and do { &tsmsg("[Wrn] Skip $glob{'curr_order'} -th COG because of too many genes [$curr_geneN]\n"); next; }; 
 
-	### Process COG 
+	### Prepare COG 
 	$glob{'curr_tab'} = [ $cog_tab[$i] ]; 
 	&prepare_ete3(); 
-	my $pref = $glob{'sepPref'}[-1]; 
-	&exeCmd_1cmd("$opts{'ete3_cmd'} -a ${pref}.prot.fa -o ${pref}_ete3O/", 0) and &stopErr("[Err] Failed to run ete3 for $glob{'curr_order'} -th COG\n"); 
+}#End for ( my $i=0; $i<@cog_tab; $i++ ) 
+
+### Run ete 
+$glob{'MAX_PROCESSES'} = $opts{'cpuN'}; 
+$glob{'pm'} = new Parallel::ForkManager($glob{'MAX_PROCESSES'}); 
+for my $sep ( @{$glob{'sepPref'}} ) {
+
+	my $has_td = ( defined $glob{'tree_dir'} ) ? 1 : 0; 
+	$has_td == 1 and $glob{'pid'} = $glob{'pm'}->start and next; 
+
+	my %tGlob = %$sep; 
+	my $pref = $tGlob{'curr_pref'}; 
+	&exeCmd_1cmd("$opts{'ete3_cmd'} -a ${pref}.prot.fa -o ${pref}_ete3O/", 0) and &stopErr("[Err] Failed to run ete3 for $tGlob{'curr_order'} -th COG\n"); 
 	opendir DD, "${pref}_ete3O/" or &stopErr("[Err] Failed to opendir [${pref}_ete3O/]\n"); 
 	for my $ff (readdir(DD)) {
 		$ff =~ m/(^\.|^db$|^tasks$)/i and next; 
@@ -71,21 +96,21 @@ for ( my $i=0; $i<@cog_tab; $i++ ) {
 			-d "$opts{'out_dir'}/$glob{'tree_dir'}/" or mkdir("$opts{'out_dir'}/$glob{'tree_dir'}/") or &stopErr("[Err] Failed to create dir [$opts{'out_dir'}/$glob{'tree_dir'}/]"); 
 		}
 		if ( $ff =~ m!^ete_build\.cfg$!i ) {
-			&fileSunhh::_move( "${pref}_ete3O/$ff", "$opts{'out_dir'}/ete_config/$glob{'curr_order'}.$ff" ); 
+			&fileSunhh::_move( "${pref}_ete3O/$ff", "$opts{'out_dir'}/ete_config/$tGlob{'curr_order'}.$ff" ); 
 		} elsif ( $ff eq $glob{'tree_dir'} ) {
 			opendir(D2, "${pref}_ete3O/$ff/") or &stopErr("[Err] Faield to opendir [${pref}_ete3O/$ff]\n"); 
 			for my $f2 ( readdir(D2) ) {
 				$f2 =~ m!^\.! and next; 
-				if ( $f2 =~ m!^$glob{'curr_order'}\.! ) {
+				if ( $f2 =~ m!^$tGlob{'curr_order'}\.! ) {
 					&fileSunhh::_move( "${pref}_ete3O/$ff/$f2", "$opts{'out_dir'}/$glob{'tree_dir'}/$f2" ); 
 				} elsif ( $f2 =~ m!^(runid|command_lines|commands.log)!i ) {
-					&fileSunhh::_move( "${pref}_ete3O/$ff/$f2", "$opts{'out_dir'}/$glob{'tree_dir'}/$glob{'curr_order'}.$f2" );
+					&fileSunhh::_move( "${pref}_ete3O/$ff/$f2", "$opts{'out_dir'}/$glob{'tree_dir'}/$tGlob{'curr_order'}.$f2" );
 				} else {
 					&stopErr("[Err] Undefined file [${pref}_ete3O/$ff/$f2] in ete output dir [${pref}_ete3O/$ff/]\n"); 
 				}
 			}
 			closedir(D2); 
-			for my $fn (@{$glob{'file_toRM'}}) {
+			for my $fn (@{$tGlob{'file_toRM'}}) {
 				unlink($fn); 
 			}
 		} else {
@@ -94,9 +119,13 @@ for ( my $i=0; $i<@cog_tab; $i++ ) {
 	}# End for my $ff (readdir(DD))
 	closedir(DD); 
 	&fileSunhh::_rmtree("${pref}_ete3O/"); 
-}#End for ( my $i=0; $i<@cog_tab; $i++ ) 
 
-&fileSunhh::_rmtree($wrk_dir); 
+	$has_td == 1 and $glob{'pm'}->finish; 
+
+}#End for my $pref () 
+$glob{'pm'}->wait_all_children; 
+
+# &fileSunhh::_rmtree($wrk_dir); 
 
 &tsmsg("[Rec] All done [$0]\n"); 
 
@@ -113,9 +142,12 @@ sub prepare_ete3 {
 			print {$glob{'curr_fh_oProt'}} ">$prot_fas{$id}{'key'}\n$prot_fas{$id}{'seq'}\n"; 
 		}
 	}
-	close( $glob{'curr_fh_oProt'} ); 
-	push(@{$glob{'sepPref'}}, $glob{'curr_pref'}); 
+	delete($glob{'curr_tab'}); 
+	close( $glob{'curr_fh_oProt'} ); delete( $glob{'curr_fh_oProt'} ); 
 	push(@{$glob{'file_toRM'}}, "$glob{'curr_pref'}.prot.fa"); 
+	my %t_glob = %glob; 
+	delete($t_glob{'sepPref'}); 
+	push( @{$glob{'sepPref'}}, \%t_glob ); 
 }#sub prepare_ete3()
 
 
