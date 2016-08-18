39cdf499365a0c32048d8341244bfd1768c8bc7a
Sunhh <biosunhh@gmail.com>
2016-08-18 14:06:56 -0400

    Add opt

--- assemble_tools/get_seq_by_kmer.pl
+++ assemble_tools/get_seq_by_kmer.pl
@@ -1,32 +1,66 @@
 #!/usr/bin/perl 
 use strict; 
 use LogInforSunhh; 
+use Getopt::Long; 
+my %opts; 
+GetOptions(\%opts, 
+	"help!", 
+	"jf_db:s", # Required. 
+	"jf_klen:i", # Default 17
+	"jf_exe:s", # Default jellyfish
+	"better_min:i", # Default 20; 
+	"better_max:i", # Default 100; 
+	"bad_max:i", # Default 200; 
+	"min_2nd_dep:i", # Default 4. 
+	"extend_len:i",  # 2000
+); 
 
-# jellyfish-2.0 query P1_Cor1st_1k_m81.jf GTCGATTAAATACGCAAGTTTGCCACTAGCTGTAGCAGCACTGTCTACGAGTCACGAGATGATGTACAAATGACCGCATAGCGCAGTGGATTAGCGCCTCTGACTTCGGATCAGAAGGTTGTGGGTTTGACTCCCACTGTGGTCATCTGT
-my $db_jf = 'P1_Cor1st_1k_m61.jf'; 
-$db_jf = 'P3_1kb_sample_m121.jf'; 
-my $exe_jf2 = '/data/share/src/Assemble/MaSuRCA/MaSuRCA-2.2.1/bin/jellyfish-2.0'; 
-my $min_2nd_dep = 1;
-$min_2nd_dep = 4;
+&_prepare_para(); 
 
-my %para; 
-$para{better_min} = 20; 
-$para{better_max} = 100; 
-$para{bad_max} = 200; 
+sub _prepare_para {
+	$opts{'jf_exe'}  //= 'jellyfish'; 
+	$opts{'jf_klen'} //= 17; 
+	$opts{'min_2nd_dep'} //= 4; 
+	$opts{'better_min'} //= 20; 
+	$opts{'better_max'} //= 100; 
+	$opts{'bad_max'} //= 200; 
+	$opts{'extend_len'} //= 2000; 
+}
 
-my $init_k = 'CATCTCGTGACTCGTAGACAGTGCTGCTACAGCTAGTGGCAAACTTGCGTATTTAATCGA'; 
-my $kl = 121; 
+my $help_txt = <<HH; 
+
+perl $0   -jf_db  in.jf_database   initial_kseq 
+
+-jf_exe       [$opts{'jf_exe'}]
+-jf_klen      [$opts{'jf_klen'}]
+-better_min   [$opts{'better_min'}]
+-better_max   [$opts{'better_max'}]
+-bad_max      [$opts{'bad_max'}]
+-min_2nd_dep  [$opts{'min_2nd_dep'}]
+-extend_len   [$opts{'extend_len'}]
+
+HH
+
+defined $opts{'jf_db'} or &LogInforSunhh::usage($help_txt); 
+$opts{'help'} and &LogInforSunhh::usage($help_txt);
+!@ARGV and &LogInforSunhh::usage($help_txt); 
+
+my $db_jf = $opts{'jf_db'}; 
+my $db_km = $opts{'jf_klen'}; 
+my $exe_jf2 = $opts{'jf_exe'}; 
+my $min_2nd_dep = $opts{'min_2nd_dep'};
+my $kl = $opts{'jf_klen'}; 
 $kl --; 
-
-$init_k = 'ACATGAAAATACATTTAAGTTCCATATGCATTTCATGACATGACTCGAGAATACTATGTGTATACATGGCATCAACCAAC'; 
-$init_k = shift; 
+my $init_k = shift; 
 $init_k = substr($init_k, 0, $kl); 
 $init_k = uc($init_k); 
 my $prev_k = $init_k; 
-
 my $elong = $init_k; 
 
-for (my $i=0; $i<2000; $i++) {
+# jellyfish-2.0 query P1_Cor1st_1k_m81.jf GTCGATTAAATACGCAAGTTTGCCACTAGCTGTAGCAGCACTGTCTACGAGTCACGAGATGATGTACAAATGACCGCATAGCGCAGTGGATTAGCGCCTCTGACTTCGGATCAGAAGGTTGTGGGTTTGACTCCCACTGTGGTCATCTGT
+
+
+for (my $i=0; $i<$opts{'extend_len'}; $i++) {
 	my @qry_kc; 
 	my $prev_k_rc = reverse($prev_k); 
 	$prev_k_rc =~ tr/ATGC/TACG/; 
@@ -55,7 +89,7 @@ for (my $i=0; $i<2000; $i++) {
 		$prev_k = substr("${prev_k}$qry_kc[0][0]", 1, $kl); 
 	} else {
 		@qry_kc = sort { $b->[1] <=> $a->[1] } @qry_kc; 
-		if ( $qry_kc[0][1] > $para{bad_max} and $qry_kc[1][1] >= $para{better_min} and $qry_kc[1][1]<= $para{better_max} ) {
+		if ( $qry_kc[0][1] > $opts{bad_max} and $qry_kc[1][1] >= $opts{better_min} and $qry_kc[1][1]<= $opts{better_max} ) {
 			&tsmsg("[Err] Multiple selection at [$i+1: $qry_kc[1][0]:$qry_kc[1][1] $qry_kc[0][0]:$qry_kc[0][1] $qry_kc[2][0]:$qry_kc[2][1]]\nkmer = ${prev_k}$qry_kc[1][0]\n"); 
 			$elong = "$elong$qry_kc[1][0]"; 
 			$prev_k = substr("${prev_k}$qry_kc[1][0]", 1, $kl); 
