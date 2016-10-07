008ac39f4d7daa90b3df2ca6ac3df22451ceac06
Sunhh <biosunhh@gmail.com>
2016-10-07 16:30:54 -0400

    Add -grpLn and -rmComment

--- run_cmd_in_batch.pl
+++ run_cmd_in_batch.pl
@@ -9,6 +9,8 @@ my %opts;
 GetOptions(\%opts, 
 	"help!", 
 	"cpuN:s", "beginLn:i", "endLn:i", "nprocF:s", 
+	"grpLn:i", 
+	"rmComment!", 
 ); 
 
 sub usage {
@@ -18,8 +20,12 @@ sub usage {
 # 
 # -beginLn       [0]
 # -endLn         [0]
-# -cpuN          1
+# -cpuN          [1]
 # -nprocF        [Nproc]
+#
+# -grpLn         [1] Do be CAREFUL when using this!!! 
+#
+# -rmComment     [Boolean]
 ################################################################################
 HH
 	exit 1; 
@@ -29,6 +35,8 @@ $opts{'beginLn'} = $opts{'beginLn'} // 0;
 $opts{'endLn'} = $opts{'endLn'} // 0; 
 $opts{'cpuN'} = $opts{'cpuN'} // 0; 
 $opts{'nprocF'} = $opts{'nprocF'} // 'Nproc'; 
+$opts{'grpLn'} //= 1; 
+$opts{'grpLn'} >= 1 or &stopErr("[Err] -grpLn cannot be smaller than 1.\n"); 
 
 -t and !@ARGV and &usage(); 
 defined $opts{'help'} and &usage(); 
@@ -39,24 +47,7 @@ while (<>) {
 	push(@joblist, $_); 
 }
 @joblist = &linearize(\@joblist); 
-sub linearize {
-	my ($in_aref) = @_; 
-	my @back; 
-	my $is_cont = 0; 
-	for my $a1 (@$in_aref) {
-		if ($is_cont == 1) {
-			$back[-1] .= "$a1"; 
-			$is_cont = 0; 
-		} else {
-			push(@back, $a1); 
-		}
-		$back[-1] !~ m!^\s*#! and $back[-1] =~ s!(\s)\\$!$1! and $is_cont = 1; 
-	}
-	if ( scalar(@back) < scalar(@$in_aref) ) {
-		@back = &linearize(\@back); 
-	}
-	return (@back); 
-}# linearize() 
+$opts{'rmComment'} and @joblist = &remove_comment(\@joblist); 
 my $ttlN = scalar(@joblist); 
 &tsmsg("[Rec] Total $ttlN lines.\n"); 
 $opts{'beginLn'} <= 0 and $opts{'beginLn'} = 1; 
@@ -68,18 +59,22 @@ my %jobDone;
 
 my $MAX_PROCESSES = $opts{'cpuN'} ; # Sometimes $parm{'cpuN'} - 1 may be better.
 my $pm = new Parallel::ForkManager($MAX_PROCESSES); 
-for (my $i=$opts{'beginLn'}-1; $i<$opts{'endLn'}; $i++) {
-	my $cmdLn = $joblist[$i]; 
+for (my $i=$opts{'beginLn'}-1; $i<$opts{'endLn'}; $i+=$opts{'grpLn'}) {
 	$MAX_PROCESSES = &change_procN($pm, $opts{'nprocF'}, $MAX_PROCESSES); 
 	my $pid = $pm->start and next; 
-	my $j=$i+1; 
-	if ( $cmdLn =~ m/^\s*$/ ) {
-		&tsmsg("[Wrn] Skip [$j] empty command : $cmdLn\n"); 
-	} elsif ( $cmdLn =~ m/^\s*\#/ ) {
-		&tsmsg("[Wrn] Skip [$j] commented command : $cmdLn\n"); 
-	} else {
-		&tsmsg("[Msg] Running [$j] command : $cmdLn\n"); 
-		&exeCmd($cmdLn); 
+	my $i1 = $i+1; 
+	for (my $j=0; $j<$opts{'grpLn'}; $j++) {
+		my $k = $i+$j; 
+		$k < $opts{'endLn'} or last; 
+		my $cmdLn = $joblist[$k]; 
+		if ( $cmdLn =~ m!^\s*$! ) {
+			&tsmsg("[Wrn] Skip [$i1+$j] empty command : $cmdLn\n"); 
+		} elsif ( $cmdLn =~ m!^\s*\#! ) {
+			&tsmsg("[Wrn] Skip [$i1+$j] commented command : $cmdLn\n"); 
+		} else {
+			&tsmsg("[Msg] Running [$i1+$j] command : $cmdLn\n"); 
+			&exeCmd_1cmd($cmdLn); 
+		}
 	}
 	$pm->finish; 
 }
@@ -107,5 +102,36 @@ sub change_procN {
 	}
 	return $new_maxP; 
 }# change_procN()
+
+
+sub linearize {
+	my ($in_aref) = @_; 
+	my @back; 
+	my $is_cont = 0; 
+	for my $a1 (@$in_aref) {
+		if ($is_cont == 1) {
+			$back[-1] .= "$a1"; 
+			$is_cont = 0; 
+		} else {
+			push(@back, $a1); 
+		}
+		$back[-1] !~ m!^\s*#! and $back[-1] =~ s!(\s)\\$!$1! and $is_cont = 1; 
+	}
+	if ( scalar(@back) < scalar(@$in_aref) ) {
+		@back = &linearize(\@back); 
+	}
+	return (@back); 
+}# linearize() 
+
+sub remove_comment {
+	my ($in_aref) = @_; 
+	my $cnt = 0; 
+	my @back; 
+	for my $a1 (@$in_aref) {
+		$a1 =~ m!^\s*(#|$)! and do { &tsmsg("[Wrn] Skip unusable line [$a1]\n"); next; }; 
+		push(@back, $a1); 
+	}
+	return(@back); 
+}# remove_comment() 
 
 
