2f458dc575d0cd7643c83c05bd93368d1c3dcdfd
Sunhh <biosunhh@gmail.com>
2016-10-19 16:27:30 -0400

    Add parameter set for .gff3 file, to avoid confused for position switch.

--- assemble_tools/cnvt_loc_fromAGP_toAGP_forLoci.pl
+++ assemble_tools/cnvt_loc_fromAGP_toAGP_forLoci.pl
@@ -15,6 +15,8 @@ GetOptions(\%opts,
 	"colN_seqID:s", 
 	"colN_seqP:s", 
 	"colN_seqStr:s", 
+	"for_gff3!", 
+	"for_vcf!", 
 	"help!", 
 ); 
 
@@ -36,11 +38,11 @@ for my $a1 (@aa_loci) {
 	$a1->[0] =~ m!^\s*(#|$)! and do { print { $glob{'fh_new_loc'} } join("\t", @$a1)."\n"; next; }; 
 	my ($new_scfID, $new_scfPos, $new_scfStr); 
 	my @new_value; 
+	my $old_scfStr = '+'; 
+	@{$glob{'colN_seqStr'}} > 0 and $old_scfStr = $a1->[$glob{'colN_seqStr'}[0]]; 
+	$old_scfStr eq '.' and $old_scfStr = '+'; 
 	for my $cN ( @{$glob{'colN_seqP'}} ) {
 		my ($old_scfID, $old_scfPos) = ( @{$a1}[ $glob{'colN_seqID'}[0], $cN ] ); 
-		my $old_scfStr = '+'; 
-		@{$glob{'colN_seqStr'}} > 0 and $old_scfStr = $a1->[$glob{'colN_seqStr'}[0]]; 
-		$old_scfStr eq '.' and $old_scfStr = '+'; 
 		my @new_scfInf = $ms_obj->transfer_position( 'from_ref2qry' => $glob{'old_s2c'}, 'to_qry2ref' => $glob{'new_c2s'}, 'fromLoc' => [$old_scfID, $old_scfPos, $old_scfStr] ); 
 		$new_scfID  //= $new_scfInf[0]; 
 		$new_scfStr //= $new_scfInf[2]; 
@@ -54,6 +56,29 @@ for my $a1 (@aa_loci) {
 	@{$glob{'colN_seqStr'}} > 0 and $a1->[ $glob{'colN_seqStr'}[0] ] = $new_value[ $glob{'colN_seqStr'}[0] ]; 
 	for my $cN ( $glob{'colN_seqID'}[0] , @{$glob{'colN_seqP'}} ) {
 		$a1->[$cN] = $new_value[ $cN ]; 
+	}
+	if ( $glob{'for_gff3'} ) {
+		if ( $a1->[3] > $a1->[4] ) {
+			$old_scfStr ne $new_scfStr or &stopErr("[Err] Strings line:\nNew:@$a1\n"); 
+			$a1->[3] > $a1->[4] and @{$a1}[3,4] = @{$a1}[4,3]; 
+		}
+	} elsif ( $glob{'for_vcf'} ) {
+		my $ref_al = $a1->[3]; 
+		my $alt_al = $a1->[4]; 
+		if ( $old_scfStr ne $new_scfStr) {
+			$ref_al =~ m!^[ATGCN]+$! or &stopErr("[Err] Bad ref allele [$ref_al]\n"); 
+			my $len = length($ref_al); 
+			$a1->[1] = $a1->[1]-$len+1; 
+			$a1->[3] =~ tr/ATGCatgc/TACGtacg/; 
+			$a1->[3] = reverse($a1->[3]); 
+			my @alt_al_a = split(/,/, $alt_al); 
+			for my $alt_al_t (@alt_al_a) {
+				$alt_al_t =~ m!^[ATGCN*]$! or &stopErr("[Err] Bad alt allele [$alt_al_t] in [$alt_al]\n"); 
+				$alt_al_t =~ tr/ATGCatgc/TACGtacg/; 
+				$alt_al_t = reverse($alt_al_t); 
+			}
+			$a1->[4] = join(',', @alt_al_a); 
+		}
 	}
 	print { $glob{'fh_new_loc'} } join("\t", @$a1)."\n"; 
 }
@@ -79,6 +104,9 @@ sub prepare_glob {
 # -colN_seqP      ['$glob{'colN_seqP'}']
 # -colN_seqStr    ['$glob{'colN_seqStr'}']
 #
+# -for_gff3       [Boolean] If given, the input and output are treated as .gff3 file, -colN_* parameters are ignored. 
+# -for_vcf        [Boolean] If given, the input and output are treated as .vcf file, -colN_* parameters are ignored. 
+#
 ################################################################################
 HH
 
@@ -99,6 +127,17 @@ HH
 	@{$glob{'colN_seqID'}}  == 1 or &stopErr("[Err] Should set one and only one column number for seqID  [-colN_seqID]\n"); 
 	@{$glob{'colN_seqStr'}} <= 1 or &stopErr("[Err] Should set one column number for seqStr at most [-colN_seqStr]\n"); 
 	@{$glob{'colN_seqP'}}   >= 1 or &stopErr("[Err] At least set one column number for seqPosition [-colN_seqP]\n"); 
+	if ($opts{'for_gff3'}) {
+		$glob{'for_gff3'} = 1; 
+		$glob{'colN_seqID'}  = [0]; 
+		$glob{'colN_seqStr'} = [6]; 
+		$glob{'colN_seqP'}   = [3,4]; 
+	} elsif ($opts{'for_vcf'}) {
+		$glob{'for_vcf'}  = 1; 
+		$glob{'colN_seqID'}  = [0]; 
+		$glob{'colN_seqStr'} = []; 
+		$glob{'colN_seqP'}   = [1]; 
+	}
 }# prepare_glob() 
 
 
