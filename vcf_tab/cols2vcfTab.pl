34ba77881f1326a243939b3eb8fd8c8dd24c7ac3
Sunhh <biosunhh@gmail.com>
2016-11-01 15:57:22 -0400

    Convert .cols.snp to .vcf.tab

---
+++ vcf_tab/cols2vcfTab.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl 
+use strict; 
+use warnings; 
+use LogInforSunhh; 
+
+-t and !@ARGV and &LogInforSunhh::usage("perl $0 in_sMao.snp > in_sMao.snp.tab\n"); 
+
+while (<>) { 
+	chomp; 
+	my @ta=split(/\t/, $_); 
+	if ( $. == 1 ) {
+		print join("\t", @ta)."\n"; 
+		next; 
+	}
+	for my $tb (@ta[3..$#ta]) { 
+		if      ( $tb =~ s!^([ATGCN*])$!$1/$1! ) {
+			; 
+		} elsif ( $tb =~ s!^([ATGC*])([ATGC*])$!$1/$2! ) {
+			; 
+		} elsif ( $tb =~ s!^([ATGC])\+([ATGCN]+)$!$1$2/$1$2!) {
+			;
+		} elsif ( $tb =~ s!^([ATGC])([ATGC])\+([ATGC]+)$!$1/$2$3! ) {
+			; 
+		} elsif ( $tb =~ m!^\+!) { 
+			$tb = "N/N"; 
+		} else { 
+			die "$tb\n"; 
+		}
+		$tb=join("/", sort ($tb =~ m!^([^/]+)/([^/]+)$!) );  
+	} 
+	print join("\t", @ta)."\n";  
+}
+
