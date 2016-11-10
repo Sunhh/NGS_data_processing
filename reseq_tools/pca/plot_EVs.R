8cb9f4f4b5009cf05de0f1f9f58f29e5df332491
Sunhh <biosunhh@gmail.com>
2016-11-10 14:49:12 -0500

    ?

--- reseq_tools/pca/plot_EVs.R
+++ reseq_tools/pca/plot_EVs.R
@@ -1,9 +1,20 @@
+#!/home/Sunhh/bin/Rscript
+
+.tsmsg <- function(...) {
+	message("[", date(), "]: ", ...)
+}# End .tsmsg
+
 argvs <- commandArgs( trailingOnly=TRUE ) ;
 
 tblFn <- argvs[1]
 legendTag <- argvs[2]
 grpLis <- argvs[-c(1,2)]
 # tblFn <- 'in_pca.set01_all.pca.evec.tbl'
+
+if ( is.null( tblFn ) || is.na( tblFn ) ) {
+	.tsmsg("Rscript plot_EVs.R in_pca.set01_all.pca.evec.tbl [ legendTag'topleft|...' group_1 group_2 group_3 group_... ]"); 
+	q(); 
+}
 
 aa <- read.table( file=tblFn, header=T, stringsAsFactors=F )
 if (length(grpLis) == 0) {
@@ -17,7 +28,7 @@ grpColor <- rainbow( grpNum )
 grpShape <- sample(1:20, size=grpNum)
 
 kk <- aa$Grp %in% grpLis
-indvColor <- rep('while', times=length(aa$Indv))
+indvColor <- rep('white', times=length(aa$Indv))
 indvShape <- rep(20,      times=length(aa$Indv))
 for ( i in 1:length(grpLis) ) {
 	indvColor[ grpLis[i] == aa$Grp ] <- grpColor[i]
