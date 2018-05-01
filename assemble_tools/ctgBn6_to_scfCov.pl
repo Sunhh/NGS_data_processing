c73abcd07ce28030818527d07e9658cfe6913afc
Sunhh <biosunhh@gmail.com>
2018-05-01 13:19:42 +0800

    fix scfSE

--- assemble_tools/ctgBn6_to_scfCov.pl
+++ assemble_tools/ctgBn6_to_scfCov.pl
@@ -12,6 +12,8 @@ GetOptions(\%opts,
 	"minIdent:f", # 0.9 
 	"log_lineN:i", # -1 
 	"topNsum:i",  # 1 
+	"keepSelfAlign!", 
+	"isPsl!", 
 ); 
 
 $opts{'minIdent'} //= 0.9; 
@@ -27,6 +29,9 @@ perl $0 Gourd969_pilon_V1.ctg2scf.agp  blastn_100k_ctg.txt
 -log_lineN    [-1]
 
 -topNsum      [1]
+
+-keepSelfAlign [Boolean] Keep alignments self to self if given. 
+-isPsl        [Boolean] The input is blat psl instead of blastn6; 
 
 HH
 
@@ -62,39 +67,73 @@ for my $ctgID (%ctg2scf) {
 my %cnt; 
 $cnt{'cntN_step'} = $opts{'log_lineN'}; 
 my %blk; 
+my %blk2; 
 while (<>) {
 	&fileSunhh::log_section( $. , \%cnt ) and &tsmsg("[Msg] Processing $. line.\n"); 
 	chomp; 
 	my @ta = split(/\t/, $_); 
-	$ta[2] >= 100 * $opts{'minIdent'} or next; 
-	defined $ctg2scf{$ta[0]} or &stopErr("[Err] No contig_ID [$ta[0]] found.\n"); 
-	my @r1s = $ms_obj->switch_position( 'qry2ref'=>\%ctg2scf , 'qryID'=>$ta[0], 'qryPos'=>$ta[6] ); 
-	my @r1e = $ms_obj->switch_position( 'qry2ref'=>\%ctg2scf , 'qryID'=>$ta[0], 'qryPos'=>$ta[7] ); 
-	$r1s[0][0] eq $r1e[0][0] or &stopErr("[Err] Bad scaffold found [$r1s[0][0]] and [$r1e[0][0]] : $_\n"); 
-	my @r2s; 
-	if (defined $ctg2scf{$ta[1]}) {
-		@r2s = $ms_obj->switch_position( 'qry2ref'=>\%ctg2scf , 'qryID'=>$ta[1], 'qryPos'=>$ta[8] ); 
+	my ($match, $mismat, $repmat, $n, $qgap, $tgap, $str, $qid,$ql,$qs,$qe, $tid,$tl,$ts,$te); 
+	my $ident; 
+	if ( $opts{'isPsl'} ) {
+		m!^(psL|\s*$|\s*match|\-+)! and next; 
+		($match, $mismat, $repmat, $n, $qgap, $tgap, $str, $qid,$ql,$qs,$qe, $tid,$tl,$ts,$te)
+		=
+		@ta[0,      1,       2,       3,  5,     7,     8,    9,   10, 11, 12,  13,  14, 15, 16]; 
+		$qs += 1; 
+		$ts += 1; 
+		$ident = 100 * $match / ( $qe-$qs+1 ) ; 
 	} else {
-		# defined $ctg2scf{$ta[1]} or &stopErr("[Err] No contig_ID [$ta[1]] found.\n"); 
-		@r2s = ([$ta[1], $ta[8], '+']); 
+		$ident = $ta[2]; 
+		($qid,$ql,$qs,$qe, $tid,$tl,$ts,$te, $str ) = @ta[ 0,12,6,7, 1,13,8,9, 14]; 
+		if (defined $str and $str ne '') {
+			if ($str =~ m!^[+-]$!) {
+				; 
+			} elsif ($str eq 'plus') {
+				$str = '+'; 
+			} elsif ($str eq 'minus') {
+				$str = '-'; 
+			} else {
+				&stopErr("[Err] Unknown str [$str]\n"); 
+			}
+		} else {
+			$str = ( $ts > $te ) ? '-' : '+' ; 
+		}
+	}
+
+	$ident >= 100 * $opts{'minIdent'} or next; 
+	defined $ctg2scf{$qid} or &stopErr("[Err] No contig_ID [$qid] found.\n"); 
+	my @r1s = $ms_obj->switch_position( 'qry2ref'=>\%ctg2scf , 'qryID'=>$qid, 'qryPos'=>$qs ); 
+	my @r1e = $ms_obj->switch_position( 'qry2ref'=>\%ctg2scf , 'qryID'=>$qid, 'qryPos'=>$qe ); 
+	$r1s[0][0] eq $r1e[0][0] or &stopErr("[Err] Bad scaffold found [$r1s[0][0]] and [$r1e[0][0]] : $_\n"); 
+	my (@r2s, @r2e); 
+	if (defined $ctg2scf{$tid}) {
+		@r2s = $ms_obj->switch_position( 'qry2ref'=>\%ctg2scf , 'qryID'=>$tid, 'qryPos'=>$ts ); 
+		@r2e = $ms_obj->switch_position( 'qry2ref'=>\%ctg2scf , 'qryID'=>$tid, 'qryPos'=>$te ); 
+	} else {
+		@r2s = ([$tid, $ts, '+']); 
+		@r2e = ([$tid, $te, '+']); 
 	}
 	my $scfID1 = $r1s[0][0]; 
 	my $scfID2 = $r2s[0][0]; 
-	$scfID1 eq $scfID2 and next; 
+	unless ( $opts{keepSelfAlign} ) {
+		$scfID1 eq $scfID2 and next; 
+	}
 	my @se1 = ( $r1s[0][1], $r1e[0][1] ); 
 	$se1[0] > $se1[1] and @se1[0,1] = @se1[1,0]; 
 	push(@{$blk{$scfID1}{$scfID2}}, [$se1[0], $se1[1]]); 
+	my @se2 = ( $r2s[0][1], $r2e[0][1] ); 
+	$se2[0] > $se2[1] and @se2[0,1] = @se2[1,0]; 
+	push(@{$blk2{$scfID2}{$scfID1}}, [$se2[0], $se2[1]]); 
+	
 }
 
-print STDOUT join("\t", qw/ScfID LenInCtg CovLenInScf CovPercInScf CovByScf CovLenSpan CovPercSpan/)."\n"; 
+print STDOUT join("\t", qw/ScfID LenInCtg CovLenInScf CovPercInScf CovByScf CovLenSpan2 CovPercSpan2 CovS2 CovE2/)."\n"; 
 for my $tk1 (sort keys %blk) {
 	my %maxSum; 
-	my @tb; 
 	
 	my %blkSum; 
 	for my $tk2 (keys %{$blk{$tk1}}) {
 		$blk{$tk1}{$tk2} = $ms_obj->mergeLocBlk( $blk{$tk1}{$tk2}, 'dist2join'=>1 ); 
-		push(@tb, $blk{$tk1}{$tk2}); 
 		my $tsum = 0; 
 		for my $ar1 ( @{ $blk{$tk1}{$tk2} } ) {
 			$tsum += $ar1->[1]-$ar1->[0]+1; 
@@ -102,15 +141,17 @@ for my $tk1 (sort keys %blk) {
 		$blkSum{$tk2} = $tsum; 
 	}
 
-	my $tc = $ms_obj->mergeLocBlk( @tb, 'dist2join'=>1 ); 
-	my $sum_span = 0; 
-	for my $ar2 (@$tc) {
-		$sum_span += $ar2->[1]-$ar2->[0]+1; 
-	}
-
 	my @srt_tk2 = sort { $blkSum{$b} <=> $blkSum{$a} } sort keys %blkSum; 
 	for (my $i=0; $i<@srt_tk2; $i++) {
 		$opts{'topNsum'} > 0 and $i >= $opts{'topNsum'} and last; 
+		my $tk2 = $srt_tk2[$i]; 
+		my $tc = $ms_obj->mergeLocBlk( $blk2{$tk2}{$tk1}, 'dist2join'=>1 ); 
+		my $sum_span = 0; 
+		for my $ar2 (@$tc) {
+			$sum_span += ($ar2->[1]-$ar2->[0]+1); 
+		}
+		my $t_s = $tc->[0][0]; 
+		my $t_e = $tc->[-1][1]; 
 		print STDOUT join("\t", 
 			$tk1, 
 			$info{$tk1}{'noN'}, 
@@ -118,7 +159,9 @@ for my $tk1 (sort keys %blk) {
 			sprintf("%0.2f", $blkSum{$srt_tk2[$i]}/$info{$tk1}{'noN'}*100), 
 			$srt_tk2[$i], 
 			$sum_span, 
-			sprintf("%0.2f", $sum_span / $info{$tk1}{'noN'}*100)
+			sprintf("%0.2f", $sum_span / ($t_e-$t_s+1) *100), 
+			$t_s, 
+			$t_e
 		)."\n"; 
 	}
 }
