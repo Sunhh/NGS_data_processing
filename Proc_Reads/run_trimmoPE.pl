fbf02580a8e630c14247c9b8f678dad985169e95
Sunhh <biosunhh@gmail.com>
2020-06-05 16:19:49 +0800

    Add file paths and better control

--- Proc_Reads/run_trimmoPE.pl
+++ Proc_Reads/run_trimmoPE.pl
@@ -15,6 +15,16 @@ my $outR2SFq = "${outPref}_sTr_R2.fq";
 
 my $exe_java = '/share/nas2/xigua/sunhonghe/src/java/jre1.8.0_131/bin/java'; 
 my $jar_trim = '/share/nas2/xigua/sunhonghe/src/reads/trimmo/Trimmomatic-0.36/trimmomatic-0.36.jar'; 
+my $dir_trim = '/Data/Sunhh/src/reads/trimmomatic/Trimmomatic-0.36/'; 
+my $fn_adp   = "$dir_trim/adapters/TruSeq3-PE-2.fa"; 
+my $para_trim = " ILLUMINACLIP:${fn_adp}\:2:30:10:1:TRUE SLIDINGWINDOW:4:20 LEADING:3 TRAILING:3 MINLEN:40 "; 
+
+$exe_java = '/usr/java/jre1.8.0_144/bin/java'; 
+$jar_trim = '/Data/Sunhh/src/reads/trimmomatic/Trimmomatic-0.36/trimmomatic-0.36.jar'; 
+$dir_trim = '/Data/Sunhh/src/reads/trimmomatic/Trimmomatic-0.36/'; 
+$fn_adp   = "$dir_trim/adapters/TruSeq3-PE-2.fa"; 
+$para_trim = " ILLUMINACLIP:${fn_adp}\:2:30:10:1:TRUE SLIDINGWINDOW:4:20 LEADING:3 TRAILING:3 MINLEN:40 "; 
+
 
 my $cmd = ""; 
 $cmd .= "$exe_java "; 
@@ -22,7 +32,7 @@ $cmd .= " -jar $jar_trim ";
 $cmd .= " PE "; 
 $cmd .= " -threads 2 "; 
 $cmd .= " $inR1Fq $inR2Fq $outR1PFq $outR1SFq $outR2PFq $outR2SFq "; 
-$cmd .= " ILLUMINACLIP:/share/nas2/xigua/sunhonghe/src/reads/trimmo/Trimmomatic-0.36/adapters/TruSeq3-PE-2.fa:2:30:10:1:TRUE SLIDINGWINDOW:4:20 LEADING:3 TRAILING:3 MINLEN:40 "; 
+$cmd .= " $para_trim "; 
 
 &exeCmd_1cmd($cmd); 
 &exeCmd_1cmd("gzip $outR1PFq $outR1SFq $outR2PFq $outR2SFq"); 
