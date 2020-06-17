e2d135525ab340753ba3d8e1e03b4b50203facbf
Sunhh <biosunhh@gmail.com>
2020-06-17 09:20:40 +0800

    Fix multi-threads output.

--- cmd_ctrl/split_scrn_time.pl
+++ cmd_ctrl/split_scrn_time.pl
@@ -6,8 +6,14 @@ use warnings;
 
 while (<>) {
 	chomp; 
-	s!^\[([^\[\]]+)\]\[([^\[\]]+)\]!! or next; 
-	my ($t1, $t2) = ($1, $2); 
-	s!^\s+!!; 
-	print join("\t", $t1, $t2, $_)."\n"; 
+	# Begin index search ...[Fri Jun 12 10:21:32 2020][CMD_done]gzip clean_files/lnc_GWRT_Rep2_*fq
+	if ( s!^\[([^\[\]]+)\]\[([^\[\]]+)\]!! ) {
+		my ($t1, $t2) = ($1, $2); 
+		s!^\s+!!; 
+		print join("\t", $t1, $t2, $_)."\n"; 
+	} elsif ( s!^.*\[(\S\S\S +\S\S\S +\d+ +\d+:\d+:\d+ +\d+)\]\[([^\[\]]+)\]!! ) {
+		my ($t1, $t2) = ($1, $2); 
+		s!^\s+!!; 
+		print join("\t", $t1, $t2, $_)."\n"; 
+	}
 }
