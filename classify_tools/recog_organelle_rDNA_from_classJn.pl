e9b6cef048e26816b8bf979e429f7aa3913eae23
Sunhh <biosunhh@gmail.com>
2022-04-15 18:10:58 -0400

    Loosen the requirement to assign rDNA and satellites.

--- classify_tools/recog_organelle_rDNA_from_classJn.pl
+++ classify_tools/recog_organelle_rDNA_from_classJn.pl
@@ -2,6 +2,7 @@
 # [4/9/2022] Need to convert this as pipeline some day later for repeat works!!!
 # [4/15/2022] Merge chloroplast and plastid to chloroplast class.
 # [4/15/2022] Try to recognize bacteria classes.
+# [4/15/2022] Try to identify dispersed hits for rDNAs and satellites.
 use strict;
 use warnings;
 use LogInforSunhh;
@@ -13,13 +14,50 @@ while (<>) {
   chomp;
   my @ta=split(/\t/, $_);
   $ta[1] eq 'SeqLen' and next;
+  # To identify rDNA and satellites:
+  if      ( $ta[5] =~ m!^rDNA:\d+(;;(Eukaryota|Satellite):\d+)*$!i ) {
+    if ( $ta[4] >= $ta[1] * 0.5 and $ta[7] < 1000 ) {
+      # I'd like to use a smaller ExMatLen/SeqLen ratio to identify rDNA related region.
+      # I'd like to require no more than 1 kb hits to In class to assure there are no genes in this contig.
+      print join("\t", "rDNA", $_)."\n"; next;
+    }
+  } elsif ( $ta[5] =~ m!^Satellite:\d+(;;(Eukaryota|rDNA):\d+)*$!i ) {
+    if ( $ta[4] >= $ta[1] * 0.5 and $ta[7] < 1000 ) {
+      print join("\t", "Satellite", $_)."\n"; next;
+    }
+  }
+  # To identify bacteria and viruses.
+  ### I'd like to use a similar way as rDNA and satellites.
+  if      ($ta[5] =~ m!^Viruses:\d+$!i) {
+    if ( $ta[4] >= $ta[1] * 0.5 and $ta[7] < 1000 ) {
+      print join("\t", "Viruses", $_)."\n"; next;
+    }
+  } elsif ($ta[5] =~ m!^Bacteria:\d+$!i) {
+    if ( $ta[4] >= $ta[1] * 0.5 and $ta[7] < 1000 ) {
+      print join("\t", "Bacteria", $_)."\n"; next;
+    }
+  } elsif ($ta[5] =~ m!^Archaea:\d+$!i) {
+    if ( $ta[4] >= $ta[1] * 0.5 and $ta[7] < 1000 ) {
+      print join("\t", "Archaea", $_)."\n"; next;
+    }
+  } elsif ($ta[5] =~ m!^NA:\d+$!i) {
+    if ( $ta[4] >= $ta[1] * 0.5 and $ta[7] < 1000 ) {
+      print join("\t", "NA", $_)."\n"; next;
+    }
+  } elsif ($ta[5] =~ m!^((?:NA|Viruses|Bacteria|Archaea):\d+(?:;;)?)+$!i) {
+    if ( $ta[4] >= $ta[1] * 0.5 and $ta[7] < 1000 ) {
+      print join("\t", "NA", $_)."\n"; next;
+    }
+  }
+
+  # To identify organelle genome and others.
   $ta[4] >= $ta[1] * 0.95 or next; # This ratio can be stricter or looser. To me 95% coverage is safe enough to lower the false positive.
-  if      ($ta[5] =~ m!^rDNA:\d+$!i) {
-    print join("\t", "rDNA", $_)."\n";
-  } elsif ($ta[5] =~ m!^((?:Chloroplast|Plastid):\d+(?:;;)?)+$!i) {
+  if      ($ta[5] =~ m!^((?:Chloroplast|Plastid):\d+(?:;;)?)+$!i) {
     print join("\t", "Chloroplast", $_)."\n";
   } elsif ($ta[5] =~ m!^Mitochondrion:\d+$!i) {
     print join("\t", "Mitochondrion", $_)."\n";
+  } elsif ($ta[5] =~ m!^rDNA:\d+$!i) {
+    print join("\t", "rDNA", $_)."\n";
   } elsif ($ta[5] =~ m!^Satellite:\d+$!i) {
     print join("\t", "Satellite", $_)."\n";
   } elsif ($ta[5] =~ m!^Viruses:\d+$!i) {
