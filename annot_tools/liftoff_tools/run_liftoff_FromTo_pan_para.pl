7ca2076a90ec21ab54834740463234f57e3544fd
Sunhh <biosunhh@gmail.com>
2022-05-14 22:28:33 -0400

    Move settings out of script.

---
+++ annot_tools/liftoff_tools/run_liftoff_FromTo_pan_para.pl
@@ -0,0 +1,78 @@
+#!/usr/bin/perl
+# [3/17/2022] Need to add the cumulative coverage length of the query gene.
+# [3/20/2022] Direction matters, so I should run bedtools twice with different directions.
+# [3/22/2022] The output gff3 of liftoff needs to be filtered because there are still gene models with gene coverage lower than required.
+#             Strand-specific overlapping has been fixed.
+# [3/23/2022] I decide not to filter coverage in this program, but I'll output the coverage bases in the output intersection file.
+use strict;
+use warnings;
+use LogInforSunhh;
+use fileSunhh;
+use Getopt::Long;
+my %opts;
+GetOptions(\%opts,
+  "from_tag:s",   # 'CLpan'
+  "from_gff3:s",  # "in.from.trim2CDS.gff3"
+  "from_fas:s",   # "in.from.genome.fa"
+  "to_tag:s",     # 'CMpan'
+  "to_gff3:s",    # "in.to.trim2CDS.gff3"
+  "to_fas:s",     # "in.to.genome.fa"
+  "out_dir:s",       # "output"
+  "para_minCov:f",   # 0.5
+  "para_minIdent:f", # 0.75
+  "para_liftoff:s",  # " -p 20 -s $opts{'para_minIdent'} -a $opts{'para_minCov'} -copies "
+  "pl_cntR2Q:s",     # "perl /home/Sunhh/tools/github/NGS_data_processing/annot_tools/liftoff_tools/cnt_R2Q_liftoff_info.pl "
+  "exe_bedtools:s",  # "bedtools"
+  "exe_liftoff:s",   # "liftoff"
+  "help!"
+);
+my $htxt = <<HH;
+####################################################################################################
+# perl $0  \\
+#   -from_tag CLpan  -from_gff3 CLpan.trim2CDS.gff3  -from_fas CLpan.genome.fa \\
+#   -to_tag   CMpan  -to_gff3   CMpan.trim2CDS.gff3  -to_fas   CMpan.genome.fa \\
+#   -out_dir  output
+# 
+# Optional parameters:
+# -para_minCov    [0.5]
+# -para_minIdent  [0.75]
+# -para_liftoff   [ -p 20 -s {para_minIdent} -a {para_minCov} -copies ]
+# -pl_cntR2Q      [perl /home/Sunhh/tools/github/NGS_data_processing/annot_tools/liftoff_tools/cnt_R2Q_liftoff_info.pl]
+# -exe_bedtools   [bedtools]
+# -exe_liftoff    [liftoff]
+####################################################################################################
+HH
+
+for my $k1 (qw/from_tag from_gff3 from_fas to_tag to_gff3 to_fas/) {
+  defined $opts{$k1} or &LogInforSunhh::usage($htxt);
+}
+$opts{'out_dir'}       //= 'output';
+$opts{'para_minCov'}   //= 0.5;
+$opts{'para_minIdent'} //= 0.75;
+$opts{'para_liftoff'}  //= " -p 20 -s $opts{'para_minIdent'} -a $opts{'para_minCov'} -copies ";
+$opts{'pl_cntR2Q'}     //= "perl /home/Sunhh/tools/github/NGS_data_processing/annot_tools/liftoff_tools/cnt_R2Q_liftoff_info.pl";
+$opts{'exe_bedtools'}  //= 'bedtools';
+$opts{'exe_liftoff'}   //= 'liftoff';
+
+my %dta;
+$dta{'tmp_dir'}  = &fileSunhh::new_tmp_dir('create' => 1);
+for (qw/para_minCov para_minIdent para_liftoff pl_cntR2Q exe_bedtools exe_liftoff/) {
+  $dta{$_} = $opts{$_};
+}
+for (qw/out_dir from_tag from_gff3 from_fas to_tag to_gff3 to_fas/) {
+  $dta{$_} = $opts{$_};
+}
+-e $dta{'out_dir'} or mkdir($dta{'out_dir'});
+$dta{'out_gff3'}  = "$dta{'out_dir'}/mapCDS.$dta{'from_tag'}.to.$dta{'to_tag'}.gff3";
+$dta{'out_tbl'}   = "$dta{'out_dir'}/mapCDS.$dta{'from_tag'}.to.$dta{'to_tag'}.tbl";
+$dta{'out_unmap'} = "$dta{'out_dir'}/mapCDS.$dta{'from_tag'}.to.$dta{'to_tag'}.unmapped";
+$dta{'toRM'} = [];
+
+&runCmd("$dta{'exe_liftoff'} $dta{'para_liftoff'} -dir $dta{'tmp_dir'} -g $dta{'from_gff3'} -o $dta{'out_gff3'} -u $dta{'out_unmap'} $dta{'to_fas'} $dta{'from_fas'}");
+&runCmd("$dta{'pl_cntR2Q'}   $dta{'from_gff3'}  $dta{'to_gff3'}  $dta{'out_gff3'} > $dta{'out_tbl'}");
+push(@{$dta{'toRM'}}, $dta{'tmp_dir'});
+
+for (@{$dta{'toRM'}}) {
+  &fileSunhh::_rmtree($_);
+}
+
