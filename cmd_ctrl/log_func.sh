6bf98e5f94b8b9e221a5dd323c48dd33985f3813
Sunhh <biosunhh@gmail.com>
2022-06-17 10:27:43 -0400

    Revise exe_cmd() function.

--- cmd_ctrl/log_func.sh
+++ cmd_ctrl/log_func.sh
@@ -1,12 +1,17 @@
 ### Basic functions.
 function exe_cmd {
-	echo "[$(date)][CMD] $1"
-	eval "$1"
-	echo "[$(date)][Rec] Done."
+  echo "[$(date)][CMD] $1"
+  eval "$1"
+  if [[ $? -eq "0" ]]
+  then
+    echo "[$(date)][CMD_done] $1"
+  else
+    echo "[$(date)][CMD_err] $1"
+  fi
 }
 
 function tsmsg {
-	echo "[$(date)]$1"
+  echo "[$(date)][Msg] $1"
 }
 
 
