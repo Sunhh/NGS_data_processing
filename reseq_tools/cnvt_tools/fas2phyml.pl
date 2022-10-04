f341ec9d2b8426c0d40e1243110f088ce4888548
Sunhh <biosunhh@gmail.com>
2022-10-04 17:30:32 -0400

    Add formats for mcmctree input.

--- reseq_tools/cnvt_tools/fas2phyml.pl
+++ reseq_tools/cnvt_tools/fas2phyml.pl
@@ -23,8 +23,9 @@ perl $0 in_aligned.fasta > out_phyml_interleaved.phyml
 
 -shrt_len       [$opts{'shrt_len'}]
 -out_id_list    ['out_id_list']
--fmt            [0] '0' for normal format; '1' for mcmctree input.
-
+-fmt            [0] '0' for normal format;
+                    '1' for mcmctree input;
+                    '2' for mcmctree input with three frames.
 
 HH
 
@@ -44,17 +45,22 @@ for (@seq_IDs) {
 my $seq_num = scalar(@seq_IDs); 
 my $seq_len = $fas_hash{ $seq_IDs[0] }{'len'}; 
 
-&tsmsg("[Msg] Printint output.\n"); 
-print STDOUT "$seq_num $seq_len\n"; 
+&tsmsg("[Msg] Printint output.\n");
+if ($opts{'fmt'} == 0 or $opts{'fmt'} == 1) {
+  print STDOUT "$seq_num $seq_len\n";
+} elsif ($opts{'fmt'} == 2) {
+  ;
+} else {
+  &stopErr("[Err] Reach here!\n");
+}
 my %used_ID; 
 
+open O,'>',"$opts{'out_id_list'}" or &stopErr("$!\n");
 if ($opts{'fmt'} == 0) {
-  open O,'>',"$opts{'out_id_list'}" or &stopErr("$!\n"); 
   for (my $i=0; $i<$seq_len; $i+=$charN_per_line) {
     if ($i==0) {
       for (@seq_IDs) {
-        my $new_ID = substr($_, 0, $opts{'shrt_len'}); 
-        $new_ID =~ s/[\s();:]/_/g; 
+        my $new_ID = substr($_, 0, $opts{'shrt_len'}); $new_ID =~ s/[\s();:]/_/g;
         my $tk = $new_ID; 
         my $suff = 'a'; 
         while ( defined $used_ID{$tk} ) {
@@ -62,11 +68,10 @@ if ($opts{'fmt'} == 0) {
           $suff ++; 
           $tk = "$new_ID$suff"; 
         }
-        $used_ID{$tk} = 1; 
-        $new_ID = $tk; 
+        $used_ID{$tk} = 1;
         print O "$tk\t$_\n"; 
         my $l2 = $opts{'shrt_len'}+1; 
-        $new_ID = sprintf("%-${l2}s", $new_ID); 
+        $new_ID = sprintf("%-${l2}s", $tk); 
         print STDOUT join(' ', $new_ID, @{ &arr_by_seg( substr($fas_hash{$_}{'seq'}, $i, $charN_per_line), 10 ) })."\n"; 
       }
       print STDOUT "\n"; 
@@ -77,9 +82,63 @@ if ($opts{'fmt'} == 0) {
     }
     print STDOUT "\n"; 
   }
-  close (O); 
-
+} elsif ($opts{'fmt'} == 1) {
+  for my $k1 (@seq_IDs) {
+    my $new_ID = substr($k1, 0, $opts{'shrt_len'}); $new_ID =~ s/[\s();:]/_/g;
+    my $tk = $new_ID;
+    my $suff = 'a';
+    while ( defined $used_ID{$tk} ) {
+      $suff eq 'z' and &stopErr("[Err] Too many repeat names [$new_ID]\n"); 
+      $suff ++; 
+      $tk = "$new_ID$suff"; 
+    }
+    $used_ID{$tk} = 1;
+    print O "$tk\t$k1\n";
+    my $l2 = $opts{'shrt_len'} + 1;
+    $new_ID = sprintf("%-${l2}s", $tk);
+    print STDOUT join(" ", $new_ID, $fas_hash{$k1}{'seq'})."\n";
+  }
+  print STDOUT "\n";
+} elsif ($opts{'fmt'} == 2) {
+  my @s2;
+  my @id2;
+  for (my $i=0; $i<@seq_IDs; $i++) {
+    my $k1 = $seq_IDs[$i];
+    my $new_ID = substr($k1, 0, $opts{'shrt_len'}); $new_ID =~ s/[\s();:]/_/g;
+    my $tk = $new_ID;
+    my $suff = 'a';
+    while ( defined $used_ID{$tk} ) {
+      $suff eq 'z' and &stopErr("[Err] Too many repeat names [$new_ID]\n"); 
+      $suff ++; 
+      $tk = "$new_ID$suff"; 
+    }
+    $used_ID{$tk} = 1;
+    print O "$tk\t$k1\n";
+    my $l2 = $opts{'shrt_len'} + 1;
+    $new_ID = sprintf("%-${l2}s", $tk);
+    push(@id2, $new_ID);
+    for (my $j=0; $j<$fas_hash{$k1}{'len'}; $j+=3) {
+      for (my $m=0; $m<3; $m++) {
+        my $jm= $j+$m;
+        $s2[$m][$i] //= "";
+        $s2[$m][$i] .= substr($fas_hash{$k1}{'seq'}, $jm, 1);
+      }
+    }
+  }
+  for (my $m=0; $m<3; $m++) {
+    my $v1 = scalar(@{$s2[$m]});
+    my $v2 = length($s2[$m][0]);
+    print STDOUT "$v1 $v2\n";
+    for (my $i=0; $i<@id2; $i++) {
+      print STDOUT join(" ", $id2[$i], $s2[$m][$i])."\n";
+    }
+    print STDOUT "\n";
+  }
+} else {
+  &stopErr("[Err] Should not arrive here!\n");
 }
+close (O); 
+
 sub arr_by_seg {
   # $_[0] whole sequence 
   # $_[1] length per segment. 
