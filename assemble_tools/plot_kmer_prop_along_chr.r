76467cf2279339eb44a1de0842865dbc2fcdef4a
Sunhh <biosunhh@gmail.com>
2023-02-04 00:33:01 -0500

    Suppress messages and allow edit tailing length for plot.

--- assemble_tools/plot_kmer_prop_along_chr.r
+++ assemble_tools/plot_kmer_prop_along_chr.r
@@ -3,18 +3,23 @@ fn <- 'kmer_cnt_chr/21QDX551/21QDX551_Chr01.tbl'
 argvs <- commandArgs( trailingOnly=TRUE );
 if ( is.na(argvs[1]) ) {
   message("###########################################################");
-  message("Rscript this.R  <kmer_cnt/Chr01.tbl>  <output_prefix>");
+  message("Rscript this.R  <kmer_cnt/Chr01.tbl>  <output_prefix> [end_length]");
   message("######");
   message("  <Chr01.tbl>: is the output of count_kmer_distr.pl\n");
   message("###########################################################");
   q();
 }
+
 fn        <- as.character(argvs[1])
 opref     <- as.character(argvs[2])
+endLen    <- 1e6
+if (!is.na(argvs[3])) {
+  endLen  <- as.integer(argvs[3])
+}
 
-library(ggplot2)
-library(dplyr)
-library(reshape2)
+suppressPackageStartupMessages(library(ggplot2))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(reshape2))
 options(bitmapType="cairo")
 
 # Set up functions.
@@ -38,7 +43,7 @@ plot1 <- function(dat, scale_txt='mb') {
   ggplot(dat, aes(x= position, y=value, fill= variable)) +
     geom_area(size= 0.5, colour= 'black', alpha= 1) +
     xlab(paste0('window start (/', scale_txt, ')')) +
-    scale_x_continuous(expand=c(0,0), breaks= brkx) +
+    scale_x_continuous(expand=c(0,0), breaks= brkx, guide= guide_axis(angle= 90) ) +
     scale_y_continuous(expand=c(0,0))
 }
 
@@ -51,19 +56,21 @@ b1 <- cbind(position=a1[,c(2)], b1)
 c1 <- melt(b1, id= "position")
 png(filename= paste0(opref, '.whole.png'), width= 480*10, type= 'cairo-png')
 plot1(dat= c1, scale_txt = 'mb')
-dev.off()
+suppressPackageStartupMessages(dev.off())
 
 # Plot first 1 mb
-b2 <- b1[b1$position <= 1e6+1, ]
+b2 <- b1[b1$position <= endLen+1, ]
 c2 <- melt(b2, id= "position")
-png(filename= paste0(opref, '.1st_1mb.png'), width=480*10, type= 'cairo-png')
+fname2 <- paste0(opref, '.1st_', round(endLen/1e6, digits=1), 'mb.png')
+png(filename= fname2, width=480*10, type= 'cairo-png')
 plot1(dat= c2, scale_txt= 'kb')
-dev.off()
+suppressPackageStartupMessages(dev.off())
 
 # Plot last 1 mb
-b3 <- b1[b1$position >= max(b1$position)-1e6+1, ]
+b3 <- b1[b1$position >= max(b1$position)-endLen+1, ]
 c3 <- melt(b3, id= "position")
-png(filename= paste0(opref, '.last_1mb.png'), width=480*10, type= 'cairo-png')
+fname3 <- paste0(opref, '.last_', round(endLen/1e6, digits=1), 'mb.png')
+png(filename= fname3, width=480*10, type= 'cairo-png')
 plot1(dat= c3, scale_txt= 'kb')
-dev.off()
+suppressPackageStartupMessages(dev.off())
 
