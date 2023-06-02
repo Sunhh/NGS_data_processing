8bb2c09b2bf3844a904aeb781b9fd03eb3dc6a6e
Sunhh <biosunhh@gmail.com>
2023-06-02 14:41:49 -0400

    Fix NM.

--- evolution_tools/SV_detection/fix_sam_cigarID.pl
+++ evolution_tools/SV_detection/fix_sam_cigarID.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 # 5/4/2023 Query must be aligned.
 # 5/30/2023 Remove ending insertions/deletions which may cause problem in NucDiff. Editing.
+# 6/2/2023: Fix NM:i:\d+ when cigar is changed.
 
 use strict;
 use warnings;
@@ -29,66 +30,86 @@ while (<>) {
     # Read left;
     ### Fix I.
     if ($ta[5] =~ s!^(\d+)H(\d+)I!!) {
-      $ta[5] = ($1+$2)."H$ta[5]";
-      $ta[9] eq '*' or substr($ta[9], 0, $2)='';
+      my ($vH,$vV)=($1,$2);
+      $ta[5] = ($vH+$vV)."H$ta[5]"; $ta[9] eq '*' or substr($ta[9], 0, $vV)='';
+      for my $tb (@ta) { $tb =~ m!^NM:i:(\d+)$! and $tb = "NM:i:".($1-$vV); }
     } elsif ($ta[5] =~ s!^(\d+)I!!) {
-      $ta[5] = $1."H$ta[5]";
-      $ta[9] eq '*' or substr($ta[9], 0, $1)='';
+      my $vV = $1;
+      $ta[5] = $vV."H$ta[5]"; $ta[9] eq '*' or substr($ta[9], 0, $vV)='';
+      for my $tb (@ta) { $tb =~ m!^NM:i:(\d+)$! and $tb = "NM:i:".($1-$vV); }
     }
     ### Fix D.
     if ($ta[5] =~ s!^(\d+)H(\d+)D!!) {
-      $ta[5] = $1."H$ta[5]";
-      $ta[3] += $2;
+      my ($vH,$vV)=($1,$2);
+      $ta[5] = $vH."H$ta[5]"; $ta[3] += $vV;
+      for my $tb (@ta) { $tb =~ m!^NM:i:(\d+)$! and $tb = "NM:i:".($1-$vV); }
     } elsif ($ta[5] =~ s!^(\d+)D!!) {
-      $ta[3] += $1;
+      my $vV = $1;
+      $ta[3] += $vV;
+      for my $tb (@ta) { $tb =~ m!^NM:i:(\d+)$! and $tb = "NM:i:".($1-$vV); }
     }
     # Read right.
     ### Fix I.
     if ($ta[5] =~ s!(\d+)I(\d+)H$!!) {
-      $ta[5] = $ta[5] . ($1+$2) . "H";
-      $ta[9] eq '*' or substr($ta[9], -$1)='';
+      my ($vV,$vH)=($1,$2);
+      $ta[5] = $ta[5] . ($vV+$vH) . "H"; $ta[9] eq '*' or substr($ta[9], -$vV)='';
+      for my $tb (@ta) { $tb =~ m!^NM:i:(\d+)$! and $tb = "NM:i:".($1-$vV); }
     } elsif ($ta[5] =~ s!(\d+)I$!!) {
-      $ta[5] = $ta[5] . $1 . "H";
-      $ta[9] eq '*' or substr($ta[9], -$1)='';
+      my $vV = $1;
+      $ta[5] = $ta[5] . $vV . "H"; $ta[9] eq '*' or substr($ta[9], -$vV)='';
+      for my $tb (@ta) { $tb =~ m!^NM:i:(\d+)$! and $tb = "NM:i:".($1-$vV); }
     }
     ### Fix D.
     if ($ta[5] =~ s!(\d+)D(\d+)H$!!) {
-      $ta[5] = $ta[5].$2."H";
+      my ($vV,$vH)=($1,$2);
+      $ta[5] = $ta[5].$vH."H";
+      for my $tb (@ta) { $tb =~ m!^NM:i:(\d+)$! and $tb = "NM:i:".($1-$vV); }
     } elsif ($ta[5] =~ s!(\d+)D$!!) {
-      ;
+      my $vV = $1;
+      for my $tb (@ta) { $tb =~ m!^NM:i:(\d+)$! and $tb = "NM:i:".($1-$vV); }
     }
   } else {
     # Reverse alignment.
     # Read left.
     ### Fix I.
     if ($ta[5] =~ s!^(\d+)H(\d+)I!!) {
-      $ta[5] = ($1+$2)."H$ta[5]";
-      $ta[9] eq '*' or substr($ta[9], 0, $2)='';
+      my ($vH,$vV)=($1,$2);
+      $ta[5] = ($vH+$vV)."H$ta[5]"; $ta[9] eq '*' or substr($ta[9], 0, $vV)='';
+      for my $tb (@ta) { $tb =~ m!^NM:i:(\d+)$! and $tb = "NM:i:".($1-$vV); }
     } elsif ($ta[5] =~ s!^(\d+)I!!) {
-      $ta[5] = $1."H$ta[5]";
-      $ta[9] eq '*' or substr($ta[9], 0, $1)='';
+      my $vV = $1;
+      $ta[5] = $vV."H$ta[5]"; $ta[9] eq '*' or substr($ta[9], 0, $vV)='';
+      for my $tb (@ta) { $tb =~ m!^NM:i:(\d+)$! and $tb = "NM:i:".($1-$vV); }
     }
     ### Fix D.
     if ($ta[5] =~ s!^(\d+)H(\d+)D!!) {
-      $ta[5] = $1."H$ta[5]";
-      $ta[3] += $2;
+      my ($vH,$vV)=($1,$2);
+      $ta[5] = $vH."H$ta[5]"; $ta[3] += $vV;
+      for my $tb (@ta) { $tb =~ m!^NM:i:(\d+)$! and $tb = "NM:i:".($1-$vV); }
     } elsif ($ta[5] =~ s!^(\d+)D!!) {
-      $ta[3] += $2;
+      my $vV = $1;
+      $ta[3] += $vV;
+      for my $tb (@ta) { $tb =~ m!^NM:i:(\d+)$! and $tb = "NM:i:".($1-$vV); }
     }
     # Read right.
     ### Fix I.
     if ($ta[5] =~ s!(\d+)I(\d+)H$!!) {
-      $ta[5] = ($1+$2)."H$ta[5]";
-      $ta[9] eq '*' or substr($ta[9], -$1)='';
+      my ($vV,$vH)=($1,$2);
+      $ta[5] = ($vV+$vH)."H$ta[5]"; $ta[9] eq '*' or substr($ta[9], -$vV)='';
+      for my $tb (@ta) { $tb =~ m!^NM:i:(\d+)$! and $tb = "NM:i:".($1-$vV); }
     } elsif ($ta[5] =~ s!(\d+)I$!!) {
-      $ta[5] = $1."H$ta[5]";
-      $ta[9] eq '*' or substr($ta[9], -$1)='';
+      my $vV = $1;
+      $ta[5] = $vV."H$ta[5]"; $ta[9] eq '*' or substr($ta[9], -$vV)='';
+      for my $tb (@ta) { $tb =~ m!^NM:i:(\d+)$! and $tb = "NM:i:".($1-$vV); }
     }
     ### Fix D.
     if ($ta[5] =~ s!(\d+)D(\d+)H$!!) {
-      $ta[5] = $ta[5] . $2 . "H";
+      my ($vV,$vH)=($1,$2);
+      $ta[5] = $ta[5] . $vH . "H";
+      for my $tb (@ta) { $tb =~ m!^NM:i:(\d+)$! and $tb = "NM:i:".($1-$vV); }
     } elsif ($ta[5] =~ s!(\d+)D$!!) {
-      ;
+      my $vV = $1;
+      for my $tb (@ta) { $tb =~ m!^NM:i:(\d+)$! and $tb = "NM:i:".($1-$vV); }
     }
   }
   print join("\t",@ta)."\n";
