4c2fdac2e3626dbb7337aa3269fe423a7ea9e3bf
Sunhh <biosunhh@gmail.com>
2023-07-05 14:48:04 -0400

    Update run_repeatmasker

--- annot_tools/repAnno_tools/run_repeatmasker.sh
+++ annot_tools/repAnno_tools/run_repeatmasker.sh
@@ -9,22 +9,20 @@ function tsmsg {
 	echo "[$(date)]$1"
 }
 
-exe_rpmk="/data/Sunhh/src/Annot/repeatmasker/RepeatMasker/RepeatMasker"
-pl_buildSum='/data/Sunhh/src/Annot/repeatmasker/RepeatMasker/util/buildSummary.pl'
+exe_rpmk="/data/Sunhh/src/annotation/repeatmasker/RepeatMasker/RepeatMasker"
+pl_buildSum="/data/Sunhh/src/annotation/repeatmasker/RepeatMasker/util/buildSummary.pl"
 
-cpuN=20
-repLib="allRepeats_v1.lib"
-seqfile="P1All.scf.fa"
-minLen=100
+cpuN=40
+repLib="D202306.TElib.fa"
+seqfile="hap1.fa"
+tsv_genom="$seqfile.tsv"
 
 tsmsg "[Rec] All start."
 
-# exe_cmd "$exe_rpmk -s -x -lib $repLib $seqfile -nolow -norna -no_is -pa $cpuN -a 1>stdout.RepMsk_unknown 2>stderr.RepMsk_unknown"
-tsv_genom='P1Genom_Gt5h.scf.fa.tsv'
+deal_fasta.pl -baseCount $seqfile | awk 'NR > 1 {print $1"\t"$8-$6}' > $tsv_genom
+
+exe_cmd "$exe_rpmk -s -x -lib $repLib $seqfile -nolow -norna -no_is -pa $cpuN -a 1>$seqfile.stdout_RepMsk 2>$seqfile.stderr_RepMsk"
 exe_cmd "perl $pl_buildSum -useAbsoluteGenomeSize -genome $tsv_genom $seqfile.out > $seqfile.out.NuclGenom.summary"
 
 tsmsg "[Rec] All done."
 
-
-
-tsmsg "[Rec] All done."
