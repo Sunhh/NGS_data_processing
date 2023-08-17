9beaa0f607e016566d8396b6fe346c9cb4866932
Sunhh <biosunhh@gmail.com>
2023-08-17 09:21:37 -0400

    Detail the SVTYPE tag.

--- evolution_tools/SV_detection/cnvt_ndfGff2vcf_struct.pl
+++ evolution_tools/SV_detection/cnvt_ndfGff2vcf_struct.pl
@@ -4,6 +4,11 @@
 #           VARs around N gaps (10 bp) are also removed.
 # 6/6/2023: I don't know why, but sometimes the reference base is same as query base in substitution.
 # 6/8/2023: Shift <INV> REF position to 1 bp left in VCF.
+# 8/15/2023: Accept SVs around 'reshuffling' segments, because I found true relocation-type DELs aside reshufflings.
+#            This can introduce many false positives in INS!!!
+# 8/16/2023: Return to previous version at 6/8/2023.
+# 8/16/2023: Change 'SVTYPE' information to ease further check.
+# 8/16/2023: Try to include query location for further check.
 
 use strict;
 use warnings;
@@ -21,11 +26,14 @@ my $fnGff = shift;
 
 my $maxDist = 10;
 
-# Possible Names:
-###  Accept: substitution;
-###  Part  : gap/inserted_gap/unaligned_end/unaligned_beginning/...;
-###  Accept: insertion/duplication/...;
-###  Accept: deletion/collapsed_repeat/collapsed_tandem_repeat/...;
+# Possible Names ($svType):
+###  Accept INS+DEL: substitution;
+###  Accept INS: insertion/duplication/tandem_duplication;
+###  Accept INS: relocation-insertion;
+###  Accept DEL: deletion/collapsed_repeat/collapsed_tandem_repeat;
+###  Accept DEL: relocation;
+###  Accept INV: inversion;
+###  Ignore: gap/inserted_gap/unaligned_end/unaligned_beginning/...;
 ###  Ignore: relocation*/translocation*/inversion/reshuffling/...;
 
 #
@@ -70,20 +78,26 @@ for my $k1 (keys %qrySeq) {
 my $fhGff = &openFH($fnGff);
 print STDOUT <<'HHHS';
 ##fileformat=VCFv4.2
-##ALT=<ID=BND,Description="Translocation">
+##ALT=<ID=BND,Description="Translocation, which should be reshuffling in NucDiff.">
 ##ALT=<ID=CNV,Description="Copy Number Polymorphism">
 ##ALT=<ID=CPX,Description="Complex SV">
 ##ALT=<ID=CTX,Description="Reciprocal chromosomal translocation">
 ##ALT=<ID=DEL,Description="Deletion">
-##ALT=<ID=DUP,Description="Duplication">
+##ALT=<ID=DEL:REP,Description="Deletion and collapsed repeat">
+##ALT=<ID=DEL:TAN,Descirption="Collapsed tandem repeat">
+##ALT=<ID=DEL:REL,Description="Relocation-shaped deletion">
+##ALT=<ID=DEL:SUB,Description="Deletion-type substitution">
 ##ALT=<ID=INS,Description="Insertion">
+##ALT=<ID=INS:DUP,Description="Duplication and/or insertion">
+##ALT=<ID=INS:TAN,Description="Tandem duplication">
+##ALT=<ID=INS:REL,Description="Relocation-insertion">
+##ALT=<ID=INS:SUB,Description="Insertion-type substitution">
+##ALT=<ID=DUP,Description="Duplication">
 ##ALT=<ID=INS:ME,Description="Mobile element insertion of unspecified ME class">
 ##ALT=<ID=INS:ME:ALU,Description="Alu element insertion">
 ##ALT=<ID=INS:ME:LINE1,Description="LINE1 element insertion">
 ##ALT=<ID=INS:ME:SVA,Description="SVA element insertion">
 ##ALT=<ID=INS:UNK,Description="Sequence insertion of unspecified origin">
-##ALT=<ID=REL,Description="Relocation">
-##ALT=<ID=RIN,Description="Relocation-insertion">
 ##ALT=<ID=INV,Description="Inversion">
 ##CPX_TYPE_NDstruct_DEL="Deletion in ref_struct.gff generated from NucDiff"
 ##CPX_TYPE_NDstruct_INS="Insertion in ref_struct.gff generated from NucDiff"
@@ -97,6 +111,7 @@ print STDOUT <<'HHHS';
 ##INFO=<ID=HOMLEN,Number=.,Type=Integer,Description="Length of base pair identical micro-homology at event breakpoints">
 ##INFO=<ID=HOMSEQ,Number=.,Type=String,Description="Sequence of base pair identical micro-homology at event breakpoints">
 ##INFO=<ID=SVLEN,Number=.,Type=Integer,Description="Difference in length between REF and ALT alleles">
+##INFO=<ID=ALTPOS,Number=1,Type=String,Description="Corresponding ALT sequence position in ChrID:start-end format. This is experimental">
 ##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
 ##FORMAT=<ID=GQ,Number=1,Type=Integer,Description="Genotype quality">
 HHHS
@@ -203,11 +218,13 @@ for my $svType (keys %svPool) {
 # Deal with special cases in which two consecutive records are used together.
 #
 # Type combinations:
-#   DEL: deletion     + collapsed_repeat
-#   INS: insertion    + duplication
-#   SUB: substitution + deletion  : Re-align.
-#   SUB: substitution + insertion : Re-align.
-#   INS: 'relocation-insertion': SV_XX.1 + SV_XX.2
+#   DEL:REP : deletion     + collapsed_repeat
+#   DEL:REL : relocation: SV_XX.1 + SV_XX.2.
+#   INS:DUP : insertion    + duplication
+#   DEL:SUB : substitution + deletion  : Re-align.
+#   INS:SUB : substitution + insertion : Re-align.
+#   INS:REL : 'relocation-insertion': SV_XX.1 + SV_XX.2
+my %new_svType;
 for my $refID (sort keys %allRefID) {
   my (@del_svIDs, @cor_svIDs, @sub_svIDs, @dup_svIDs, @ins_svIDs, @rin_svIDs, @rel_svIDs);
   defined $refID2svID{'deletion'}{$refID} and @del_svIDs = @{$refID2svID{'deletion'}{$refID}};
@@ -221,7 +238,7 @@ for my $refID (sort keys %allRefID) {
   @rel_svIDs = map { s!\.1!!; $_; } @rel_svIDs;
   my %rm_del_idx;
   my %rm_ins_idx;
-  # DEL: deletion     + collapsed_repeat
+  # DEL:REP : deletion     + collapsed_repeat
   for my $cor_ID (@cor_svIDs) {
     my ($cor_rS, $cor_rE, $cor_qID, $cor_qS, $cor_qE, $cor_str) = @{$svPool{'collapsed_repeat'}{$refID}{$cor_ID}};
     for (my $i=0; $i<@del_svIDs; $i++) {
@@ -230,16 +247,18 @@ for my $refID (sort keys %allRefID) {
       my ($del_rS, $del_rE, $del_qID, $del_qS, $del_qE, $del_str) = @{$svPool{'deletion'}{$refID}{$del_ID}};
       $del_rE < $cor_rS and next;
       $del_rS > $cor_rE and last;
-      $cor_str == 1  and $del_str == 1  and $del_rS == $cor_rS and do {$rm_del_idx{$i}=1; last;};
-      $cor_str == -1 and $del_str == -1 and $del_rE == $cor_rE and do {$rm_del_idx{$i}=1; last;};
+      $cor_str == 1  and $del_str == 1  and $del_rS == $cor_rS and do {$rm_del_idx{$i}=1; $new_svType{$cor_ID}='DEL:REP'; last;};
+      $cor_str == -1 and $del_str == -1 and $del_rE == $cor_rE and do {$rm_del_idx{$i}=1; $new_svType{$cor_ID}='DEL:REP'; last;};
     }
   }
-  # DEL: 'relocation': SV_XX.1 + SV_XX.2. Update location and strand.
+  # DEL:REL : 'relocation': SV_XX.1 + SV_XX.2. Update location and strand.
   for my $rel_ID (@rel_svIDs) {
     my $rel_ID1 = "$rel_ID.1";
     my $rel_ID2 = "$rel_ID.2";
     my ($r1_rS, $r1_rE, $r1_qID, $r1_qS, $r1_qE, $r1_str) = @{$svPool{'relocation'}{$refID}{$rel_ID1}};
     my ($r2_rS, $r2_rE, $r2_qID, $r2_qS, $r2_qE, $r2_str) = @{$svPool{'relocation'}{$refID}{$rel_ID2}};
+    $new_svType{$rel_ID1} = 'DEL:REL';
+    $new_svType{$rel_ID2} = 'DEL:REL';
     if ($r1_rS > $r2_rS) {
       # Deleted ref region should be [$r2_rE+1, $r1_rS-1]
       $svPool{'relocation'}{$refID}{$rel_ID1}[5] = -1;
@@ -252,7 +271,7 @@ for my $refID (sort keys %allRefID) {
   }
 
   
-  # INS: insertion    + duplication
+  # INS:DUP : insertion    + duplication
   for my $dup_ID (@dup_svIDs) {
     my ($dup_rS, $dup_rE, $dup_qID, $dup_qS, $dup_qE, $dup_str) = @{$svPool{'duplication'}{$refID}{$dup_ID}};
     for (my $i=0; $i<@ins_svIDs; $i++) {
@@ -261,14 +280,19 @@ for my $refID (sort keys %allRefID) {
       my ($ins_rS, $ins_rE, $ins_qID, $ins_qS, $ins_qE, $ins_str) = @{$svPool{'insertion'}{$refID}{$ins_ID}};
       $ins_rE < $dup_rS and next;
       $ins_rS > $dup_rE and last;
-      $ins_rS == $dup_rS and $dup_str*$ins_str == 1 and do {$rm_ins_idx{$i}=1; $svPool{'duplication'}{$refID}{$dup_ID}[3]=$ins_qS; last;};
+      if ( $ins_rS == $dup_rS and $dup_str*$ins_str == 1 ) {
+        $rm_ins_idx{$i}=1;
+        $svPool{'duplication'}{$refID}{$dup_ID}[3]=$ins_qS;
+        $new_svType{$dup_ID} = 'INS:DUP';
+        last;
+      }
     }
   }
 
   CHK_COMB_SUB:
   for my $sub_ID (@sub_svIDs) {
     my ($sub_rS, $sub_rE, $sub_qID, $sub_qS, $sub_qE, $sub_str) = @{$svPool{'substitution'}{$refID}{$sub_ID}};
-    # SUB: substitution + deletion  : Re-align. OR record directly. Re-align them later.
+    # DEL:SUB : substitution + deletion  : Re-align. OR record directly. Re-align them later.
     for (my $i=0; $i<@del_svIDs; $i++) {
       defined $rm_del_idx{$i} and next;
       my $del_ID = $del_svIDs[$i];
@@ -277,14 +301,16 @@ for my $refID (sort keys %allRefID) {
       if ($sub_str == 1 and $del_str == 1 and $del_rS == $sub_rE+1) {
         $rm_del_idx{$i}=1;
         $svPool{'substitution'}{$refID}{$sub_ID}[1] = $del_rE;
+        $new_svType{$sub_ID} = 'DEL:SUB';
         next CHK_COMB_SUB;
       } elsif ($sub_str == -1 and $del_str == -1 and $del_rE == $sub_rS-1) {
         $rm_del_idx{$i}=1;
         $svPool{'substitution'}{$refID}{$sub_ID}[0] = $del_rS;
+        $new_svType{$sub_ID} = 'DEL:SUB';
         next CHK_COMB_SUB;
       }
     }
-    # SUB: substitution + insertion : Re-align. OR record long sequence. Re-align them later in VCF files.
+    # INS:SUB : substitution + insertion : Re-align. OR record long sequence. Re-align them later in VCF files.
     for (my $i=0; $i<@ins_svIDs; $i++) {
       defined $rm_ins_idx{$i} and next;
       my $ins_ID = $ins_svIDs[$i];
@@ -293,20 +319,24 @@ for my $refID (sort keys %allRefID) {
       if ($sub_str == 1 and $ins_str == 1 and $ins_rS == $sub_rE) {
         $rm_ins_idx{$i}=1;
         $svPool{'substitution'}{$refID}{$sub_ID}[4] = $ins_qE;
+        $new_svType{$sub_ID} = 'INS:SUB';
         next CHK_COMB_SUB;
       } elsif ($sub_str == -1 and $ins_str == -1 and $ins_rE+1 == $sub_rS) {
         $rm_ins_idx{$i}=1;
         $svPool{'substitution'}{$refID}{$sub_ID}[4] = $ins_qE;
+        $new_svType{$sub_ID} = 'INS:SUB';
         next CHK_COMB_SUB;
       }
     }
   }
-  # INS: 'relocation-insertion': SV_XX.1 + SV_XX.2. Update location and strand.
+  # INS:REL : 'relocation-insertion': SV_XX.1 + SV_XX.2. Update location and strand.
   for my $rin_ID (@rin_svIDs) {
     my $rin_ID1 = "$rin_ID.1";
     my $rin_ID2 = "$rin_ID.2";
     my ($r1_rS, $r1_rE, $r1_qID, $r1_qS, $r1_qE, $r1_str) = @{$svPool{'relocation-insertion'}{$refID}{$rin_ID1}};
     my ($r2_rS, $r2_rE, $r2_qID, $r2_qS, $r2_qE, $r2_str) = @{$svPool{'relocation-insertion'}{$refID}{$rin_ID2}};
+    $new_svType{$rin_ID1} = 'INS:REL';
+    $new_svType{$rin_ID2} = 'INS:REL';
     if ($r1_rS > $r2_rS) {
       $svPool{'relocation-insertion'}{$refID}{$rin_ID1}[5] = -1;
       $svPool{'relocation-insertion'}{$refID}{$rin_ID2}[5] = -1;
@@ -315,7 +345,7 @@ for my $refID (sort keys %allRefID) {
       $svPool{'relocation-insertion'}{$refID}{$rin_ID2}[5] = 1;
     }
   }
-  # Clean deletion/insertion SVs.
+  # Clean deletion SVs.
   my (@new_del, @new_ins);
   for (my $i=0; $i<@del_svIDs; $i++) {
     if (defined $rm_del_idx{$i}) {
@@ -325,6 +355,7 @@ for my $refID (sort keys %allRefID) {
     }
   }
   @{$refID2svID{'deletion'}{$refID}} = @new_del;
+  # Clean insertion SVs.
   for (my $i=0; $i<@ins_svIDs; $i++) {
     if (defined $rm_ins_idx{$i}) {
       delete $svPool{'insertion'}{$refID}{$ins_svIDs[$i]};
@@ -344,29 +375,30 @@ for my $refID (sort keys %allRefID) {
     my ($rS, $rE, $qryID, $qS, $qE, $strN) = @{$svPool{'deletion'}{$refID}{$var_ID}};
     my $baseRef = substr($refSeq{$refID}{'seq'}, $rS-2, $rE-$rS+2);
     my $baseAlt = substr($baseRef, 0, 1);
-    my $infoTxt = "ALGORITHMS=NucDiff_struct;SVTYPE=DEL;END=$rE;SVLEN=-".($rE-$rS+1);
+    my $infoTxt = "ALGORITHMS=NucDiff_struct;SVTYPE=DEL;END=$rE;SVLEN=-".($rE-$rS+1).";ALTPOS=$qryID:$qS-$qE";
     &boundary_inBad($refID, $rS, $rE, $qryID, $qS, $qE, $maxDist) and next;
     print STDOUT join("\t", $refID, $rS-1, '.', $baseRef, $baseAlt, '.', 'PASS', $infoTxt, "GT", "1/1")."\n";
   }
-  # Output collapsed_repeat
+  # Output collapsed_repeat (DEL:REP)
   for my $var_ID (@{$refID2svID{'collapsed_repeat'}{$refID}}) {
     my ($rS, $rE, $qryID, $qS, $qE, $strN) = @{$svPool{'collapsed_repeat'}{$refID}{$var_ID}};
     my $baseRef = substr($refSeq{$refID}{'seq'}, $rS-2, $rE-$rS+2);
     my $baseAlt = substr($baseRef, 0, 1);
-    my $infoTxt = "ALGORITHMS=NucDiff_struct;SVTYPE=DEL;END=$rE;SVLEN=-".($rE-$rS+1);
+    my $t1 = (defined $new_svType{$var_ID}) ? $new_svType{$var_ID} : 'DEL:REP:UNK' ;
+    my $infoTxt = "ALGORITHMS=NucDiff_struct;SVTYPE=$t1;END=$rE;SVLEN=-".($rE-$rS+1).";ALTPOS=$qryID:$qS-$qE";
     &boundary_inBad($refID, $rS, $rE, $qryID, $qS, $qE, $maxDist) and next;
     print STDOUT join("\t", $refID, $rS-1, '.', $baseRef, $baseAlt, '.', 'PASS', $infoTxt, "GT", "1/1")."\n";
   }
-  # Output collapsed_tandem_repeat (DEL)
+  # Output collapsed_tandem_repeat (DEL:TAN)
   for my $var_ID (@{$refID2svID{'collapsed_tandem_repeat'}{$refID}}) {
     my ($rS, $rE, $qryID, $qS, $qE, $strN) = @{$svPool{'collapsed_tandem_repeat'}{$refID}{$var_ID}};
     my $baseRef = substr($refSeq{$refID}{'seq'}, $rS-2, $rE-$rS+2);
     my $baseAlt = substr($baseRef, 0, 1);
-    my $infoTxt = "ALGORITHMS=NucDiff_struct;SVTYPE=DEL;END=$rE;SVLEN=-".($rE-$rS+1);
+    my $infoTxt = "ALGORITHMS=NucDiff_struct;SVTYPE=DEL:TAN;END=$rE;SVLEN=-".($rE-$rS+1).";ALTPOS=$qryID:$qS-$qE";
     &boundary_inBad($refID, $rS, $rE, $qryID, $qS, $qE, $maxDist) and next;
     print STDOUT join("\t", $refID, $rS-1, '.', $baseRef, $baseAlt, '.', 'PASS', $infoTxt, "GT", "1/1")."\n";
   }
-  # Output relocation (DEL)
+  # Output relocation (DEL:REL)
   for my $var_ID (grep { $_ =~ m!\.1$! } @{$refID2svID{'relocation'}{$refID}}) {
     my ($rS, $rE, $qryID, $qS, $qE, $strN) = @{$svPool{'relocation'}{$refID}{$var_ID}};
     my $rel_ID2 = $var_ID; $rel_ID2 =~ s!\.1$!.2!;
@@ -375,7 +407,8 @@ for my $refID (sort keys %allRefID) {
     $strN == -1 and ($rSx, $rEx) = ($rE2+1, $rS-1);
     my $baseRef = substr($refSeq{$refID}{'seq'}, $rSx-2, $rEx-$rSx+2);
     my $baseAlt = substr($baseRef, 0, 1);
-    my $infoTxt = "ALGORITHMS=NucDiff_struct;SVTYPE=REL;END=$rEx;SVLEN=-".($rEx-$rSx+1);
+    my $t1 = (defined $new_svType{$var_ID}) ? $new_svType{$var_ID} : 'DEL:REL:UNK' ;
+    my $infoTxt = "ALGORITHMS=NucDiff_struct;SVTYPE=$t1;END=$rEx;SVLEN=-".($rEx-$rSx+1).";ALTPOS=$qryID:$qS-$qE";
     &boundary_inBad($refID, $rS, $rE, $qryID, $qS, $qE, $maxDist) and next;
     print STDOUT join("\t", $refID, $rSx-1, '.', $baseRef, $baseAlt, '.', 'PASS', $infoTxt, "GT", "1/1")."\n";
   }
@@ -386,33 +419,34 @@ for my $refID (sort keys %allRefID) {
     my $baseAlt = substr($qrySeq{$qryID}{'seq'}, $qS-1, $qE-$qS+1);
     $strN == -1 and &fastaSunhh::rcSeq(\$baseAlt, 'rc');
     $baseAlt = $baseRef . $baseAlt;
-    my $infoTxt = "ALGORITHMS=NucDiff_struct;SVTYPE=INS;END=".($rS+1).";SVLEN=".($qE-$qS+1);
+    my $infoTxt = "ALGORITHMS=NucDiff_struct;SVTYPE=INS;END=".($rS+1).";SVLEN=".($qE-$qS+1).";ALTPOS=$qryID:$qS-$qE";
     &boundary_inBad($refID, $rS, $rE, $qryID, $qS, $qE, $maxDist) and next;
     print STDOUT join("\t", $refID, $rS, '.', $baseRef, $baseAlt, '.', 'PASS', $infoTxt, "GT", "1/1")."\n";
   }
-  # Output duplication
+  # Output duplication (INS:DUP)
   for my $var_ID (@{$refID2svID{'duplication'}{$refID}}) {
     my ($rS, $rE, $qryID, $qS, $qE, $strN) = @{$svPool{'duplication'}{$refID}{$var_ID}};
     my $baseRef = substr($refSeq{$refID}{'seq'}, $rS-1, 1);
     my $baseAlt = substr($qrySeq{$qryID}{'seq'}, $qS-1, $qE-$qS+1);
     $strN == -1 and &fastaSunhh::rcSeq(\$baseAlt, 'rc');
     $baseAlt = $baseRef . $baseAlt;
-    my $infoTxt = "ALGORITHMS=NucDiff_struct;SVTYPE=INS;END=".($rS+1).";SVLEN=".($qE-$qS+1);
+    my $t1 = (defined $new_svType{$var_ID}) ? $new_svType{$var_ID} : 'INS:DUP:UNK' ;
+    my $infoTxt = "ALGORITHMS=NucDiff_struct;SVTYPE=$t1;END=".($rS+1).";SVLEN=".($qE-$qS+1).";ALTPOS=$qryID:$qS-$qE";
     &boundary_inBad($refID, $rS, $rE, $qryID, $qS, $qE, $maxDist) and next;
     print STDOUT join("\t", $refID, $rS, '.', $baseRef, $baseAlt, '.', 'PASS', $infoTxt, "GT", "1/1")."\n";
   }
-  # Output tandem_duplication (INS)
+  # Output tandem_duplication (INS:TAN)
   for my $var_ID (@{$refID2svID{'tandem_duplication'}{$refID}}) {
     my ($rS, $rE, $qryID, $qS, $qE, $strN) = @{$svPool{'tandem_duplication'}{$refID}{$var_ID}};
     my $baseRef = substr($refSeq{$refID}{'seq'}, $rS-1, 1);
     my $baseAlt = substr($qrySeq{$qryID}{'seq'}, $qS-1, $qE-$qS+1);
     $strN == -1 and &fastaSunhh::rcSeq(\$baseAlt, 'rc');
     $baseAlt = $baseRef . $baseAlt;
-    my $infoTxt = "ALGORITHMS=NucDiff_struct;SVTYPE=INS;END=".($rS+1).";SVLEN=".($qE-$qS+1);
+    my $infoTxt = "ALGORITHMS=NucDiff_struct;SVTYPE=INS:TAN;END=".($rS+1).";SVLEN=".($qE-$qS+1).";ALTPOS=$qryID:$qS-$qE";
     &boundary_inBad($refID, $rS, $rE, $qryID, $qS, $qE, $maxDist) and next;
     print STDOUT join("\t", $refID, $rS, '.', $baseRef, $baseAlt, '.', 'PASS', $infoTxt, "GT", "1/1")."\n";
   }
-  # Output relocation-insertion
+  # Output relocation-insertion (INS:REL)
   for my $var_ID (grep { $_ =~ m!\.1$! } @{$refID2svID{'relocation-insertion'}{$refID}}) {
     my ($rS, $rE, $qryID, $qS, $qE, $strN) = @{$svPool{'relocation-insertion'}{$refID}{$var_ID}};
     my ($rS1, $rE1, $qryID1, $qS1, $qE1, $strN1) = ($rS, $rE, $qryID, $qS, $qE, $strN);
@@ -423,29 +457,31 @@ for my $refID (sort keys %allRefID) {
     my $baseAlt = substr($qrySeq{$qryID}{'seq'}, $qS-1, $qE-$qS+1);
     $strN == -1 and &fastaSunhh::rcSeq(\$baseAlt, 'rc');
     $baseAlt = $baseRef . $baseAlt;
-    my $infoTxt = "ALGORITHMS=NucDiff_struct;SVTYPE=RIN;END=".($rS+1).";SVLEN=".($qE-$qS+1);
+    my $t1 = (defined $new_svType{$var_ID}) ? $new_svType{$var_ID} : 'INS:REL:UNK' ;
+    my $infoTxt = "ALGORITHMS=NucDiff_struct;SVTYPE=$t1;END=".($rS+1).";SVLEN=".($qE-$qS+1).";ALTPOS=$qryID:$qS-$qE";
     &boundary_inBad($refID, $rS1, $rE1, $qryID, $qS1, $qE1, $maxDist) and next;
     &boundary_inBad($refID, $rS2, $rE2, $qryID, $qS2, $qE2, $maxDist) and next;
     print STDOUT join("\t", $refID, $rS, '.', $baseRef, $baseAlt, '.', 'PASS', $infoTxt, "GT", "1/1")."\n";
   }
-  # Output inversion
+  # Output inversion (INV)
   for my $var_ID (@{$refID2svID{'inversion'}{$refID}}) {
     my ($rS, $rE, $qryID, $qS, $qE, $strN) = @{$svPool{'inversion'}{$refID}{$var_ID}};
     # my $baseRef = substr($refSeq{$refID}{'seq'}, $rS-1, $rE-$rS+1);
     my $baseRef = substr($refSeq{$refID}{'seq'}, $rS-2, 1); # Inverted region is [$rS, $rE], labeled at $rS-1.
     my $baseAlt = '<INV>';
-    my $infoTxt = "ALGORITHMS=NucDiff_struct;SVTYPE=INV;END=$rE;SVLEN=0";
+    my $infoTxt = "ALGORITHMS=NucDiff_struct;SVTYPE=INV;END=$rE;SVLEN=0".";ALTPOS=$qryID:$qS-$qE";
     &boundary_inBad($refID, $rS, $rE, $qryID, $qS, $qE, $maxDist) and next;
     print STDOUT join("\t", $refID, $rS-1, '.', $baseRef, $baseAlt, '.', 'PASS', $infoTxt, "GT", "1/1")."\n";
   }
-  # Output substitution
+  # Output substitution (DEL:SUB / INS:SUB)
   for my $var_ID (@{$refID2svID{'substitution'}{$refID}}) {
     my ($rS, $rE, $qryID, $qS, $qE, $strN) = @{$svPool{'substitution'}{$refID}{$var_ID}};
     my $baseRef = substr($refSeq{$refID}{'seq'}, $rS-1, $rE-$rS+1);
     my $baseAlt = substr($qrySeq{$qryID}{'seq'}, $qS-1, $qE-$qS+1);
     $strN == -1 and &fastaSunhh::rcSeq(\$baseAlt, 'rc');
     $baseRef eq $baseAlt and next;
-    my $infoTxt = "ALGORITHMS=NucDiff_struct;SVTYPE=SUB;END=$rE;SVLEN=".($qE-$qS-$rE+$rS);
+    my $t1 = (defined $new_svType{$var_ID}) ? $new_svType{$var_ID} : 'UNK:SUB' ;
+    my $infoTxt = "ALGORITHMS=NucDiff_struct;SVTYPE=$t1;END=$rE;SVLEN=".($qE-$qS-$rE+$rS).";ALTPOS=$qryID:$qS-$qE";
     &boundary_inBad($refID, $rS, $rE, $qryID, $qS, $qE, $maxDist) and next;
     print STDOUT join("\t", $refID, $rS, '.', $baseRef, $baseAlt, '.', 'PASS', $infoTxt, "GT", "1/1")."\n";
   }
