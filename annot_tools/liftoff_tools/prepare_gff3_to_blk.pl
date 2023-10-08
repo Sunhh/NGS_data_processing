e1b80599b398bd6d590dfbc499f3f578946762f9
Sunhh <biosunhh@gmail.com>
2023-10-08 10:42:33 -0400

    Add two columns to blk file

--- annot_tools/liftoff_tools/prepare_gff3_to_blk.pl
+++ annot_tools/liftoff_tools/prepare_gff3_to_blk.pl
@@ -1,19 +1,26 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use LogInforSunhh;
 
 !@ARGV and die "perl $0  CLpan  CLpan.trim2CDS.gff3.JnLoc > CLpan.trim2CDS.blk\n";
 
 my $tag = shift;
 my $fn_JnLoc = shift;
 
+$tag ne '' and $tag .= ':';
+
 open F,'<', $fn_JnLoc or die;
 while (<F>) {
   chomp;
   my @ta=split(/\t/, $_);
   $ta[0] eq 'mrnaID' and next;
-  print STDOUT join("\t", "$tag:$ta[0]", @ta[2,5,9])."\n";
+  my @se = split(/;/, $ta[9]);
+  my $cdsL = 0;
+  for my $tb (@se) {
+    $tb =~ m!^(\d+)\,(\d+)$! or die "[Err] bad blk format |$tb|\n";
+    $cdsL += ($2-$1+1);
+  }
+  print STDOUT join("\t", "$tag$ta[0]", @ta[2,5,9], '.', $cdsL)."\n";
 }
 close F;
 
