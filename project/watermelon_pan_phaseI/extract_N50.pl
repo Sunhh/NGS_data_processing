cd8c86c26403bcda9fd935cdcd5f5e700059aeaf
Sunhh <biosunhh@gmail.com>
2024-07-04 09:50:10 -0400

    Add more levels.

--- project/watermelon_pan_phaseI/extract_N50.pl
+++ project/watermelon_pan_phaseI/extract_N50.pl
@@ -4,21 +4,27 @@ use warnings;
 
 !@ARGV and die "perl $0 in.fa.N50 in_2.fa.N50 > out.table\n";
 
-print join("\t", qw/Accession asm_size asm_num asm_N50 asm_N90 gnm_size GN50 GN90/)."\n";
-my @oKey = qw/ttl_bp ttl_num N50 N90 estG_bp GN50 GN90/;
+my @oKey = qw/asm_size asm_num longest asm_N25 asm_N50 asm_N90 asm_N95 asm_N99 shortest gnm_size GN50 GN90/;
+print join("\t", 'accession', @oKey)."\n";
 for my $fn (@ARGV) {
   my %h;
   open F,'<',"$fn" or die;
   while (<F>) {
-    m!^Total sequences number.*:\s*(\d+)! and $h{'ttl_num'} //= $1;
-    m!^Total sequences bp \(ATGC\):\s*(\d+)! and $h{'ttl_bp'} //= $1;
-    m!^N50.+:\s*(\d+)! and $h{'N50'} //= $1;
-    m!^N90.+:\s*(\d+)! and $h{'N90'} //= $1;
-    m!^Est\. Genome size\s*:\s*(\d+)! and $h{'estG_bp'} = $1;
-    m!^NG50.+:\s*(\d+)! and $h{'GN50'} = $1;
-    m!^NG90.+:\s*(\d+)! and $h{'GN90'} = $1;
+    m!^Total sequences number.*:\s*(\d+)! and $h{'asm_num'} //= $1;
+    m!^Total sequences bp \(ATGC\):\s*(\d+)! and $h{'asm_size'} //= $1;
+    m!^Maximum length \(ATGC\)\s*:\s*(\d+)! and $h{'longest'} //= $1;
+    m!^Minimum length \(ATGC\)\s*:\s*(\d+)! and $h{'shortest'} //= $1;
+    m!^N25.+:\s*(\d+)! and $h{'asm_N25'} //= $1;
+    m!^N50.+:\s*(\d+)! and $h{'asm_N50'} //= $1;
+    m!^N90.+:\s*(\d+)! and $h{'asm_N90'} //= $1;
+    m!^N95.+:\s*(\d+)! and $h{'asm_N95'} //= $1;
+    m!^N99.+:\s*(\d+)! and $h{'asm_N99'} //= $1;
+    m!^Est\. Genome size\s*:\s*(\d+)! and $h{'gnm_size'} //= $1;
+    m!^NG50.+:\s*(\d+)! and $h{'GN50'} //= $1;
+    m!^NG90.+:\s*(\d+)! and $h{'GN90'} //= $1;
   }
   close F;
+  $h{'gnm_size'} == 0 and $h{'gnm_size'} = 'NA';
   for my $k1 (@oKey) {
     $h{$k1} //= 'NA';
   }
