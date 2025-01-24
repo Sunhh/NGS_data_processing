59e1e03fc14d39f06bea2e59a7713024f4f7265e
Sunhh <biosunhh@gmail.com>
2025-01-24 12:38:31 -0500

    Update BSA method.

---
+++ reseq_tools/bsa/README.md
@@ -0,0 +1,97 @@
+# Pipeline to do BSA analysis
+
+## Call variants into VCF file with GATK.
+In this pipeline, no parental sequence data is required.
+- Input files: 
+ - Read files: `lowBulk_R1.fq.gz`, `lowBulk_R2.fq.gz`, `highBulk_R1.fq.gz`, `highBulk_R2.fq.gz`;
+ - Reference genome file: `db/ref_chr.fa` and related BWA indexes.
+ - Software: `bwa`, `samtools`, `sentieon` for GATK;
+ - Other information: How many individuals were sampled in `low` and `high` bulks for sequencing.
+ 
+- Output files:
+ - Filtered VCF file: `two_bulks.vcf.gz`
+
+
+```sh
+mkdir bam/ gvcf/ vcf/;
+
+bwa mem -M -t 30 db/ref_chr.fa  lowBulk_R1.fq.gz  lowBulk_R2.fq.gz  -R "@RG\tID:lowBulk\tSM:lowBulk\tPL:Illumina\tLB:lowBulk\tPU:NA"    | samtools sort -@ 10 -m 10G -o bam/lowBulk.bam
+bwa mem -M -t 30 db/ref_chr.fa  highBulk_R1.fq.gz highBulk_R2.fq.gz -R "@RG\tID:highBulk\tSM:highBulk\tPL:Illumina\tLB:highBulk\tPU:NA" | samtools sort -@ 10 -m 10G -o bam/highBulk.bam
+
+samtools index -@ 20 -b bam/lowBulk.bam
+samtools index -@ 20 -b bam/highBulk.bam
+
+# steps: 2- Get metrics; 3- Deduplication; 4- BQSR; 5- GVCF;
+cat lis1 | perl -e 'use fileSunhh; use LogInforSunhh;
+my $exeSen = "/data/shared/sentieon-genomics-202308.03/bin/sentieon";
+&runCmd("rm -f cx2_metrics cx3_dedup cx4_bqsr cx5_gvcf");
+my $fa = "/data/Sunhh/wmhifi/analysis/snp_related/gatk_snp/db/ref_chr.fa";
+while (<>) {chomp;
+
+ my $p="bam/$_";
+ my $p1="gvcf/$_";
+ &fileSunhh::write2file("cx2_metrics", "$exeSen driver -t 10 -r $fa -i $p.bam --algo AlignmentStat ${p}_stat.txt\n", ">>");
+ &fileSunhh::write2file("cx3_dedup", "$exeSen driver -t 100 -i $p.bam --algo LocusCollector --fun score_info $p.SCORE\n", ">>");
+ &fileSunhh::write2file("cx3_dedup", "$exeSen driver -t 100 -i $p.bam --algo Dedup --rmdup --score_info $p.SCORE --metrics $p.dedup-metric.txt $p.dedup.bam\n", ">>");
+ &fileSunhh::write2file("cx4_bqsr", "$exeSen driver -t 100 -r $fa -i $p.dedup.bam --algo QualCal $p.recal.table\n", ">>");
+ &fileSunhh::write2file("cx5_gvcf", "$exeSen driver -t 30 -r $fa -i $p.dedup.bam --algo Haplotyper --emit_mode gvcf $p1.gvcf.gz\n", ">>");
+}'
+
+
+# step 6- Combine GVCF;
+cat lis2 | perl -e 'use fileSunhh; use LogInforSunhh;
+my $exeSen = "/data/shared/sentieon-genomics-202308.03/bin/sentieon";
+&runCmd("rm -f cx6_call");
+my $fa = "/data/Sunhh/wmhifi/analysis/snp_related/gatk_snp/db/ref_chr.fa";
+my @x1 = <>; chomp(@x1);
+my @fG = map {"gvcf/$_.gvcf.gz"} @x1;
+&fileSunhh::write2file("cx6_call", "$exeSen driver -t 90 -r $fa --algo GVCFtyper vcf/noRecal.vcf.gz @fG\n", ">>");
+&fileSunhh::write2file("cx6_call", "$exeSen driver -t 90 -r $fa --algo GVCFtyper --emit_conf 1 vcf/noRecalEmQ1.vcf.gz @fG\n", ">>");
+'
+
+# step 7- hard filter;
+mv vcf/noRecal.vcf.gz vcf/emQ30.vcf.gz
+bcftools index --tbi --threads 90 vcf/emQ30.vcf.gz
+
+docker run --rm -v $PWD:/mnt broadinstitute/gatk:4.6.1.0 gatk SelectVariants -R /mnt/db/ref_chr.fa -V /mnt/vcf/emQ30.vcf.gz --select-type-to-include SNP -O /mnt/vcf/emQ30-gatkSNP.vcf.gz
+docker run --rm -v $PWD:/mnt broadinstitute/gatk:4.6.1.0 rm -f /mnt/vcf/emQ30-gatkSNP.vcf.gz.tbi
+bcftools index --tbi --threads 90 vcf/emQ30-gatkSNP.vcf.gz
+bcftools view -e "QD < 2.0 || FS > 60.0 || MQ < 40.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0 || AC == AN || AC == 0" vcf/emQ30-gatkSNP.vcf.gz --threads 90 | bcftools +fill-tags --threads 90 -Oz -o vcf/emQ30-gatkSNPhard.vcf.gz
+bcftools index --tbi --threads 90 vcf/emQ30-gatkSNPhard.vcf.gz
+docker run --rm -v $PWD:/mnt broadinstitute/gatk:4.6.1.0 gatk SelectVariants -R /mnt/db/ref_chr.fa -V /mnt/vcf/emQ30.vcf.gz --select-type-to-include INDEL -O /mnt/vcf/emQ30-gatkInDel.vcf.gz
+docker run --rm -v $PWD:/mnt broadinstitute/gatk:4.6.1.0 rm -f /mnt/vcf/emQ30-gatkInDel.vcf.gz.tbi
+bcftools index --tbi --threads 90 vcf/emQ30-gatkInDel.vcf.gz
+bcftools view -e "QD < 2.0 || FS > 200.0 || ReadPosRankSum < -20.0 || AC == AN || AC == 0" vcf/emQ30-gatkInDel.vcf.gz --threads 90 | bcftools +fill-tags --threads 90 -Oz -o vcf/emQ30-gatkInDelhard.vcf.gz
+bcftools index --tbi --threads 90 vcf/emQ30-gatkInDelhard.vcf.gz
+
+bcftools concat -a -Oz -o vcf/emQ30-gatkAllhard.vcf.gz --threads 90 vcf/emQ30-gatkSNPhard.vcf.gz vcf/emQ30-gatkInDelhard.vcf.gz
+bcftools index --tbi --threads 90 vcf/emQ30-gatkAllhard.vcf.gz
+
+# Futher filtering:
+### Used rules: bi-allelic, not on CLV01_Chr00/Ct/Mt
+### Not used rules: No neighboring variants;
+bcftools view -t ^CLV01_Chr00,CLV01_ChrMt,CLV01_ChrCt -c2 -C2 vcf/emQ30-gatkAllhard.vcf.gz -Oz -o vcf/emQ30-gatkAllhard-use.vcf.gz
+bcftools index --tbi --threads 90 vcf/emQ30-gatkAllhard-use.vcf.gz
+
+```
+
+
+## Perform BSA with R/QTLseqr;
+- Prepare table file from VCF for R/QTLseqr.
+ - Keep only high and low bulks.
+- Output file: `vcf/use-2bulk-chrV.table`
+```sh
+bcftools view vcf/emQ30-gatkAllhard-use.vcf.gz -s lowBulk,highBulk -Ov -o vcf/use-2bulk.vcf
+docker run --rm -v $PWD:/mnt broadinstitute/gatk:4.6.1.0 gatk VariantsToTable -R /mnt/db/ref_chr.fa -V /mnt/vcf/use-2bulk.vcf \
+ -F CHROM -F POS -F REF -F ALT \
+ -GF AD -GF DP -GF GQ -GF PL \
+ -O /mnt/vcf/use-2bulk.table
+perl -e 'while (<>) { s!^CLV01_Chr0*!!; s!^\t!0\t!; print;}' vcf/use-2bulk.table > vcf/use-2bulk-chrV.table
+```
+
+- Perform BSA.
+```sh
+Rscript run_QTLseqr.r -i vcf/use-2bulk-chrV.table --high_bulk highBulk --low_bulk lowBulk --indvN_high 20 --indvN_low 20 --window_size 2000000 --window_name 2M --plot_chr 6 -o output
+```
+
+
---
+++ reseq_tools/bsa/scripts/run_QTLseqr.r
@@ -0,0 +1,174 @@
+#!/usr/bin/env Rscript
+
+suppressMessages({ library(optparse) });
+
+# Manually corrrect this variable to include all wanted chromosome names!!!
+Chroms   <- 1:11;
+# window_sizes <- c(1e6, 2e6, 500e3);
+# window_size_IDs <- c('1M', '2M', '500K')
+
+# Define command-line options
+option_list <- list(
+  make_option(c("-i", "--in_vcf_table"), type = "character", help = "GATK VCF-derived table with only high and low bulk samples", metavar = "FILES"),
+  make_option("--high_bulk", type = "character", help = "Sample ID of high bulk"),
+  make_option("--low_bulk", type = "character", help = "Sample ID of low bulk"),
+  make_option("--indvN_high", type = "numeric", help = "Sequenced sample number in high bulk", default= 30),
+  make_option("--indvN_low", type = "numeric", help = "Sequenced sample number in low bulk", default= 30),
+  make_option("--window_size", type = "numeric", help = "Length of window size", default= 1e6),
+  make_option("--window_name", type = "character", help = "Label of window size", default= "1M"),
+  make_option("--plot_chr",    type= "character", help = "ID of one chromosome to be plotted"),
+  make_option(c("-o", "--output_prefix"), type = "character", help = "Prefix of output files.", default= "out")
+)
+
+# Parse command-line arguments
+opt_parser <- OptionParser(option_list = option_list)
+opt <- parse_args(opt_parser)
+
+if (is.null(opt$in_vcf_table) || is.null(opt$high_bulk) || is.null(opt$low_bulk)) {
+  print_help(opt_parser)
+  stop("\nMandatory arguments missing! Provide phenotypic data files and group definitions.\n")
+}
+
+# rawData <- 'vcf/use-2bulk-chrV.table'
+# HighBulk <- 'hongchi'
+# LowBulk  <- 'fenchi'
+
+# bulk_size_High <- 20
+# bulk_size_Low  <- 20
+
+
+suppressMessages({
+library(QTLseqr)
+library(ggplot2)
+library(dplyr)
+});
+
+
+#import data
+df <- importFromGATK(
+ file = opt$in_vcf_table,
+ highBulk = opt$high_bulk,
+ lowBulk = opt$low_bulk,
+ chromList = Chroms
+)
+
+# Quality control.
+
+### total read depth;
+pdf(file= paste0(opt$output_prefix, '-depth_ttl.pdf'))
+ggplot(data = df) +
+ geom_histogram(aes(x = DP.HIGH + DP.LOW)) +
+ xlim(0,200)
+dev.off()
+pdf(file= paste0(opt$output_prefix, '-depth_high.pdf'))
+ggplot(data = df) +
+ geom_histogram(aes(x = DP.HIGH)) +
+ xlim(0,200)
+dev.off()
+pdf(file= paste0(opt$output_prefix, '-depth_low.pdf'))
+ggplot(data = df) +
+ geom_histogram(aes(x = DP.LOW)) +
+ xlim(0,200)
+dev.off()
+### total reference allele frequency
+pdf(file= paste0(opt$output_prefix, '-ref_AF_ttl.pdf'))
+ggplot(data = df) + geom_histogram(aes(x = REF_FRQ))
+dev.off()
+### SNP-index for high bulk.
+pdf(file= paste0(opt$output_prefix, '-SNPidx_high.pdf'))
+ggplot(data = df) + geom_histogram(aes(x = SNPindex.HIGH))
+dev.off()
+### SNP-index for low bulk.
+pdf(file= paste0(opt$output_prefix, '-SNPidx_low.pdf'))
+ggplot(data = df) + geom_histogram(aes(x = SNPindex.LOW))
+dev.off()
+
+### Filter SNPs by depth
+df_filt <- filterSNPs(
+ SNPset = df,
+ refAlleleFreq = 0.10,
+ minTotalDepth = 20,
+ maxTotalDepth = 200,
+ depthDifference = 66,
+ minSampleDepth = 40,
+ minGQ = 99,
+ verbose = TRUE
+)
+
+# Run BSA analysis.
+
+  df_filt_ana <- df_filt # Duplicate data to avoid changing origin data.
+
+  # Run analysis:
+  ### runQTLseqAnalysis: performs Takagi et al type QTLseq analysis
+  df_filt_ana <- runQTLseqAnalysis(df_filt_ana,
+   windowSize = opt$window_size,
+   popStruc = "F2",
+   bulkSize = c(opt$indvN_high, opt$indvN_low),
+   replications = 10000,
+   intervals = c(95, 99)
+  )
+  ### runGprimeAnalysis: performs Magwene et al type G′ analysis
+  df_filt_ana <- runGprimeAnalysis(df_filt_ana,
+   windowSize = opt$window_size,
+   outlierFilter = "deltaSNP",
+   filterThreshold = 0.1
+  )
+  
+  pdf(file= paste0(opt$output_prefix, '-ana-', opt$window_name, '.pdf'), width= 14, height= 7)
+  ### Plot G prime distribution: Is it close to log-normal distribution?
+  plotGprimeDist(SNPset = df_filt_ana, outlierFilter = "Hampel") + xlim(0, 10)
+  plotGprimeDist(SNPset = df_filt_ana, outlierFilter = "deltaSNP", filterThreshold = 0.1) + xlim(0,10)
+  ### plot the SNP/window distribution along chromosome.
+  p1 <- plotQTLStats(SNPset = df_filt_ana, var = "nSNPs")
+  print(p1);
+  ### plot the confidence intervals to identify QTL using G prime method.
+  p2 <- plotQTLStats(SNPset = df_filt_ana, var = "Gprime", plotThreshold = TRUE, q = 0.01)
+  print(p2)
+  ### plot the confidence intervals to identify QTL using QTL seq method.
+  p3 <- plotQTLStats(SNPset = df_filt_ana, var = "deltaSNP", plotIntervals = TRUE) + 
+    theme(
+      panel.grid.major = element_blank(), # Remove grids.
+      panel.grid.minor = element_blank()  # Remove grids.
+    )
+  print(p3)
+  if (!is.null(opt$plot_chr)) {
+    ### Subset plot
+    ##### QTLseqr
+    p4 <- plotQTLStats(
+     SNPset = df_filt_ana,
+     var = "deltaSNP",
+     plotIntervals = TRUE,
+     plotThreshold = FALSE,
+     q = 0.01,
+     subset = opt$plot_chr
+    )
+    print(p4)
+    ##### Gprime
+    p5 <- plotQTLStats(
+     SNPset = df_filt_ana,
+     var = "negLog10Pval",
+     plotThreshold = TRUE,
+     q = 0.01,
+     subset = opt$plot_chr
+    )
+    print(p5)
+  }
+  dev.off()
+ 
+
+  # Extract QTL data.
+  res_qtl_var  <- getSigRegions(SNPset = df_filt_ana, alpha = 0.01, method= 'QTLseq')
+  res_qtl_var_merged <- bind_rows(res_qtl_var)
+  res_qtl_loci <- getQTLTable(SNPset = df_filt_ana, method = "QTLseq", interval = 99, export = FALSE) # 6   1 23817744 25756406 1938662;
+  res_gpr_var  <- getSigRegions(SNPset = df_filt_ana, alpha = 0.01, method= 'Gprime')
+  res_gpr_var_merged <- bind_rows(res_gpr_var)
+  res_gpr_loci <- getQTLTable(SNPset = df_filt_ana, method = "Gprime", interval = 99, export = FALSE) # 6   1 23817744 25756406 1938662;
+
+  # Output tables.
+  write.table(x= df_filt_ana,  file= paste0(opt$output_prefix, '-ana-', opt$window_name, '-data.tab'), append = FALSE, quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
+  write.table(x= res_qtl_loci, file= paste0(opt$output_prefix, '-ana-', opt$window_name, '-QTLseq_loci.tab'), append = FALSE, quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
+  write.table(x= res_qtl_var_merged, file= paste0(opt$output_prefix, '-ana-', opt$window_name, '-QTLseq_vars.tab'), append = FALSE, quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
+  write.table(x= res_gpr_loci, file= paste0(opt$output_prefix, '-ana-', opt$window_name, '-Gprime_loci.tab'), append = FALSE, quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
+  write.table(x= res_gpr_var_merged, file= paste0(opt$output_prefix, '-ana-', opt$window_name, '-Gprime_vars.tab'), append = FALSE, quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
+
