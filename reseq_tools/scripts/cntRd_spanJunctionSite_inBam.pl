aef2f46381b64eb3b79a6186ab653691e36f3bf7
Sunhh <biosunhh@gmail.com>
2025-02-18 20:32:50 -0500

    Allow genotyping for HiFi read alignment.

--- reseq_tools/scripts/cntRd_spanJunctionSite_inBam.pl
+++ reseq_tools/scripts/cntRd_spanJunctionSite_inBam.pl
@@ -13,6 +13,7 @@ GetOptions(\%opts,
   "positive_control_loc:s", # duplicate:131502-131503
   "flank_len:i",             # 2000;
   "max_insertion_len:i",     # 2000;
+  "is_hifird!",
   "help!",
 );
 # >duplicate [seq_source=CLV01_Chr01:607964-740465] [break_point=739465-739466]
@@ -23,6 +24,7 @@ perl $0 -in_bam in.bam -target_loc duplicate:66241-66262  -positive_control_loc 
 
 -help
 -flank_len         [2000]
+-is_hifird         [Boolean]
 
 HH
 
@@ -43,6 +45,9 @@ my $pos_seg_e = $pos_e + $opts{'flank_len'};
 
 
 my %flag_goodR = %{ &SeqAlnSunhh::mk_flag("keep"=>"0=1,2=0,3=0,4=1,5=0") };
+if ($opts{'is_hifird'}) {
+  %flag_goodR = %{ &SeqAlnSunhh::mk_flag("keep"=>"0=0,2=0,8=0") };
+}
 my $fn = $opts{'in_bam'};
 
 my $tgt_cnt = 0;
@@ -52,12 +57,21 @@ open F1,"-|", "samtools view $fn $tgt_id:$tgt_seg_s-$tgt_seg_e" or die "$!\n";
 while (<F1>) {
   chomp;
   my @ta=split(/\t/, $_);
-  $ta[6] eq "=" or next;
+  if ($opts{'is_hifird'}) {
+    $ta[6] eq "*" or next;
+  } else {
+    $ta[6] eq "=" or next;
+  }
   defined $flag_goodR{$ta[1]} or next;
   my $rP_s = $ta[7];
+  if ($opts{'is_hifird'}) {
+    $rP_s = $ta[3];
+  }
   my ($rd_len, $ref_span) = &SeqAlnSunhh::cigar_array2len( &SeqAlnSunhh::cigar_str2array( $ta[5] ) );
   my $rP_e = $ta[3] + $ref_span - 1;
-  $rP_e-$rP_s+1 <= $opts{'max_insertion_len'} or next;
+  unless ($opts{'is_hifird'}) {
+    $rP_e-$rP_s+1 <= $opts{'max_insertion_len'} or next;
+  }
   if ($rP_s <= $tgt_s and $rP_e >= $tgt_e) {
     $tgt_cnt ++;
   }
@@ -68,12 +82,21 @@ open F2,"-|", "samtools view $fn $pos_id:$pos_seg_s-$pos_seg_e " or die "$!\n";
 while (<F2>) {
   chomp;
   my @ta=split(/\t/, $_);
-  $ta[6] eq "=" or next;
+  if ($opts{'is_hifird'}) {
+    $ta[6] eq "*" or next;
+  } else {
+    $ta[6] eq "=" or next;
+  }
   defined $flag_goodR{$ta[1]} or next;
   my $rP_s = $ta[7];
+  if ($opts{'is_hifird'}) {
+    $rP_s = $ta[3];
+  }
   my ($rd_len, $ref_span) = &SeqAlnSunhh::cigar_array2len( &SeqAlnSunhh::cigar_str2array( $ta[5] ) );
   my $rP_e = $ta[3] + $ref_span - 1;
-  $rP_e-$rP_s+1 <= $opts{'max_insertion_len'} or next;
+  unless ($opts{'is_hifird'}) {
+    $rP_e-$rP_s+1 <= $opts{'max_insertion_len'} or next;
+  }
   if ($rP_s <= $pos_s and $rP_e >= $pos_e) {
     $pos_cnt ++;
   }
