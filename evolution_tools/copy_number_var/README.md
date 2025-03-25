26639fbd2102926695b7b8468e2b11d652c5a84f
Sunhh <biosunhh@gmail.com>
2025-03-25 16:49:22 -0400

    update

--- evolution_tools/copy_number_var/README.md
+++ evolution_tools/copy_number_var/README.md
@@ -1,12 +1,20 @@
 # Gene CNV analysis.
 
-## Use FET test
+## Test by FET.
 - Prepare orthologous group count file. (`synFam.cnt`). First line shows accession names.
 - Prepare two-column meta file projecting accessions to populations. (`map.acc_pop`)
 - Test presence frequency change between two populations.
 
 ```sh
-Rscript compare_gene_expansion.r -a synFam.cnt -b map.acc_pop  -f landrace   -t cultivar  -o landrace_to_cultivar.CNV.tbl
+Rscript compare_gene_expansion.r -a synFam.cnt -b map.acc_pop  -f landrace    -t cultivar  -o landrace_to_cultivar.CNV.tbl
+Rscript compare_gene_expansion.r -a synFam.cnt -b map.acc_pop  -f cordophanus -t landrace  -o cordophanus_to_landrace.CNV.tbl
 ```
 
+## Combine test results.
+```sh
+echo -e "landrace_to_cultivar.CNV.tbl\land_cult" > meta.comparison_name
+echo -e "cordophanus_to_landrace.CNV.tbl\tCLC_land" >> meta.comparison_name
+
+perl combine_DEGs.pl meta.comparison_name 1 > combined.CNV.tbl
+```
 
--- rnaseq_tools/map_to_genome/combine_DEGs.pl
+++ rnaseq_tools/map_to_genome/combine_DEGs.pl
@@ -3,8 +3,12 @@ use strict;
 use warnings;
 
 # Check input
-die "Usage: $0 meta_file > combined_output.tsv\n" unless @ARGV == 1;
+die "Usage: $0 meta_file  [columns:2-3,5] > combined_output.tsv\n" unless @ARGV >= 1;
+
 my $meta_file = $ARGV[0];
+my @coln;
+scalar(@ARGV) > 1 and @coln = &parseCol($ARGV[1]);
+for (@coln) { $_--; $_ >= 0 or die "Cannot add 0-col\n"; }
 
 # Store file paths and labels
 my @files;
@@ -24,32 +28,40 @@ close META;
 my %data;
 my @gene_ids;
 my $header_found = 0;
+my ($first_name, @header_names);
 
 # Read each DEG file
 foreach my $file (@files) {
-    open IN, "<", $file or die "Cannot open $file: $!";
-    my $header = <IN>; # Skip header line
-    while (<IN>) {
-        chomp;
-        my ($gene, @vals) = split /\t/;
-        push @gene_ids, $gene if !$header_found;
-        push @{ $data{$gene} }, @vals;
-    }
-    $header_found = 1;
-    close IN;
+  open IN, "<", $file or die "Cannot open $file: $!";
+  my $header = <IN>;
+  chomp($header);
+  ($first_name, @header_names) = split(/\t/, $header);
+  if (scalar(@coln) == 0) {
+    @coln = (0 .. $#header_names);
+  }
+  @header_names = @header_names[@coln];
+  while (<IN>) {
+    chomp;
+    my ($gene, @vals) = split /\t/;
+    @vals = @vals[@coln];
+    push @gene_ids, $gene if !$header_found;
+    push @{ $data{$gene} }, @vals;
+  }
+  $header_found = 1;
+  close IN;
 }
 
 # Print headers
-print "GeneID";
+print "$first_name";
 foreach my $label (@labels) {
-    print "\t$label\t$label\t$label\t$label";
+  print "\t$label" x scalar(@coln);
 }
 print "\n";
 
 # Print sub-header
-print "GeneID";
+print "$first_name";
 foreach my $f (@files) {
-    print "\tMeanTPM_Baseline\tMeanTPM_Treatment\tFDR\tLog2FoldChange";
+  print "\t".join("\t", @header_names);
 }
 print "\n";
 
@@ -59,3 +71,25 @@ foreach my $gene (@gene_ids) {
     print "\t", join("\t", @{ $data{$gene} });
     print "\n";
 }
+
+sub parseCol {
+  my @cols = split(/,/, $_[0]);
+  my @ncols;
+  for my $tc (@cols) {
+    $tc =~ s/(^\s+|\s+$)//g;
+    if ($tc =~ m/^\d+$/) {
+      push(@ncols, $tc);
+    } elsif ($tc =~ m/^(\d+)\-(\d+)$/) {
+      my ($s, $e) = ($1, $2);
+      if ($s <= $e) {
+        push(@ncols, ($s .. $e));
+      }else{  
+        push(@ncols, reverse($e .. $s));
+      }
+    } else {
+      die("[Err]Unparsable column tag for [$tc]\n");
+    }
+  }
+  return (@ncols);
+}
+
