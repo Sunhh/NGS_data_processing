1240b479dc224849e4947f97cdb472d998fe7861
Sunhh <biosunhh@gmail.com>
2026-03-26 13:05:10 -0400

    Revise class names.

--- assemble_tools/busco/geneCopyN_busco_full_table.pl
+++ assemble_tools/busco/geneCopyN_busco_full_table.pl
@@ -10,17 +10,19 @@ while (<>) {
   m!^\s*$! and next; 
   chomp; 
   my @ta=split(/\t/, $_); 
-  if ($ta[1] eq "Complete" or $ta[1] eq "Duplicated") {
-    $h{"complete"}{$ta[0]} ++; 
+  if ($ta[1] eq "Complete") {
+    $h{"Complete"}{$ta[0]} ++; 
+  } elsif ($ta[1] eq "Duplicated") {
+    $h{"Duplicated"}{$ta[0]} ++; 
   } elsif ($ta[1] eq "Fragmented") {
-    $h{"fragmented"}{$ta[0]} ++; 
+    $h{"Fragmented"}{$ta[0]} ++; 
   } elsif ($ta[1] eq "Missing") {
-    $h{"missing"}{$ta[0]} ++; 
+    $h{"Missing"}{$ta[0]} ++; 
   } else {
     die "unknown tag [$ta[1]]\n"; 
   }
 }
-for my $k1 (qw/complete fragmented missing/) {
+for my $k1 (qw/Complete Duplicated Fragmented Missing/) {
   $h{$k1} //= {}; 
   for my $k2 (sort keys %{$h{$k1}}) {
     print join("\t", $k2, $k1, $h{$k1}{$k2})."\n"; 
